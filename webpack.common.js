1e53e246d012acc3262e888c0a5337818678d10a
puffingcheeks <nikita.pietrov.71@mail.ru>
2018-05-01 17:55:08 +1000

    babel env

--- webpack.common.js
+++ webpack.common.js
@@ -51,7 +51,6 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: 'babel-loader',
                 query: {
-                    presets: ['react', 'es2015', 'react-hmre', 'env'],
                     cacheDirectory: true,
                 }
             },
