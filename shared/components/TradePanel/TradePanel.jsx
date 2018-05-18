68d1f6c3be9633f1085a717997547f63493b5043
Pavel Ivanov <grammka@gmail.com>
2018-05-18 13:38:58 +0300

    update trade panel markup

--- shared/components/TradePanel/TradePanel.jsx
+++ shared/components/TradePanel/TradePanel.jsx
@@ -1,21 +1,36 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import './TradePanel.scss'
 
 import Title from './Title/Title'
 import Input from './Input/Input'
 
-export default function TradePanel({ name, currency, row, styleName, className }) {
-  return (
-    <div className={`${styleName} ${className}`}>
-      <Title name={name} />
-      <Input currency={currency} row={row} />
+const TradePanel = ({ name, currency, row, className }) => (
+  <div className={className} >
+    <Title name={name} />
+    <div className="trade-panel__group">
+      <input type="number" placeholder="0" className="trade-panel__input" />
+      <div className="trade-panel__label trade-panel__label_row">
+        <label className="btn-group trade-panel__select">
+          <span className="trade-panel__icon icon-btc" />
+          <select
+            className="trade-panel__search-field"
+            dir="rtl"
+          >
+            <option className="dropdown-item"><span className="trade-panel__icon icon-btc" /> BTC</option>
+            <option className="dropdown-item"><span className="trade-panel__icon icon-eth" /> ETH</option>
+          </select>
+        </label>
+      </div>
     </div>
-  )
-}
+  </div>
+)
 
 TradePanel.propTypes = {
   name: PropTypes.string.isRequired,
   currency: PropTypes.string.isRequired,
   row: PropTypes.string,
-  styleName: PropTypes.string.isRequired,
+  className: PropTypes.string.isRequired,
 }
+
+export default TradePanel
