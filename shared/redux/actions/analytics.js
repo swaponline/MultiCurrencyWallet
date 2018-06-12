25e767da887fdfa6c4c76a55f402bf5bcdef0c30
Rustam <trubnikov.r@lsit.ru>
2018-06-12 15:04:18 +0500

    #43

--- shared/redux/actions/analytics.js
+++ shared/redux/actions/analytics.js
@@ -2,19 +2,17 @@ import reducers from 'redux/core/reducers'
 import TagManager from 'react-gtm-module'
 
 const addEvent = (name) => reducers.addEvent(name) //@ToDo ???
-
-
-
 const dataEvent = (eventName) => {
 
   window.dataLayer = window.dataLayer ? window.dataLayer : [];
   window.dataLayer.push({'event' : eventName});
 
-  console.log(window.dataLayer)
+  console.log(window.dataLayer)//@ToDo delete
 }
 
 const tagManagerArgs = {
-  gtmId: 'GTM-000000'
+  gtmId: 'GTM-WK72GSV',
+  dataLayerName: 'dataLayer'
 }
 
 TagManager.initialize(tagManagerArgs)
