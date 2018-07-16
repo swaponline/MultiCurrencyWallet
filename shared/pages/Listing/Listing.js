0dc8b40e70cd511cffa18683dd97a8460fc79847
nthgness <wu-rza@mail.ru>
2018-07-16 20:56:22 +0300

    add listing page (links associate)

---
+++ shared/pages/Listing/Listing.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+
+export default class Listing extends Component {
+  render() {
+    // const { address } = this.props
+    // console.log(address)
+    return (
+ 		<div className="container">
+ 			<h2>Crypto Pink Sheets: ALL Coins Welcome To SWAP.Online!</h2>	
+ 		  	<p>OTC-approach-crypto market to be opened in Aug, 2018.</p>
+ 		</div>
+    )
+  }
+}
