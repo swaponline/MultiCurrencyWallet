30a86865ed1cd57ba4e674265d6edc7f9da8da06
nikdementev <nikita.pietrov.71@mail.ru>
2018-07-26 00:04:18 +0300

    fixed pull request

--- shared/components/FeedBack/FeedBack.js
+++ shared/components/FeedBack/FeedBack.js
@@ -1,23 +1,23 @@
-import React, { Fragment } from 'react'
-import cssModules from 'react-css-modules'
+import React from 'react'
+
 import styles from './Feedback.scss'
+import cssModules from 'react-css-modules'
+import TelegramImg from './images/telegram.png'
 
 import Href from 'components/Href/Href'
 
-import TelegramImg from './images/telegram.png'
 
-
-const FeedBack = ({link}) => (
-    <div styleName="FeedBack">
-       <h2 styleName="heading">Contacts</h2> 
-       <div styleName="contact">
-           <img src={TelegramImg} alt="" />
-           <Href tab={link}>{link}</Href> 
-           <span styleName="text">or</span>
-           <Href tab="mailto:team@swap.online">team@swap.online</Href> 
-           <span styleName="text">to find out further actions</span>
-       </div>
+const FeedBack = ({ link }) => (
+  <div styleName="FeedBack">
+    <h2 styleName="heading">Contacts</h2>
+    <div styleName="contact">
+      <img src={TelegramImg} alt="" />
+      <Href tab={link}>{link}</Href>
+      <span styleName="text">or</span>
+      <Href tab="mailto:team@swap.online">team@swap.online</Href>
+      <span styleName="text">to find out further actions</span>
     </div>
+  </div>
 )
 
 export default cssModules(FeedBack, styles)
