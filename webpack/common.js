f4926a651f2daa20bdfed02227fee0805787983f
Denis S. Soldatov aka General-Beck <general.beck@gmail.com>
2018-07-31 19:15:47 +0300

    fix base PATH for swap.core

--- webpack/common.js
+++ webpack/common.js
@@ -37,13 +37,13 @@ const webpackConfig = {
   resolve: {
     alias: {
       shared: config.paths.base('shared'),
-      'swap.auth': config.paths.swapCore('swap.core/src/swap.auth'),
-      'swap.orders': config.paths.swapCore('swap.core/src/swap.orders'),
-      'swap.room': config.paths.swapCore('swap.core/src/swap.room'),
-      'swap.app': config.paths.swapCore('swap.core/src/swap.app'),
-      'swap.flows': config.paths.swapCore('swap.core/src/swap.flows'),
-      'swap.swap': config.paths.swapCore('swap.core/src/swap.swap'),
-      'swap.swaps': config.paths.swapCore('swap.core/src/swap.swaps'),
+      'swap.auth': config.paths.swapCore('src/swap.auth'),
+      'swap.orders': config.paths.swapCore('src/swap.orders'),
+      'swap.room': config.paths.swapCore('src/swap.room'),
+      'swap.app': config.paths.swapCore('src/swap.app'),
+      'swap.flows': config.paths.swapCore('src/swap.flows'),
+      'swap.swap': config.paths.swapCore('src/swap.swap'),
+      'swap.swaps': config.paths.swapCore('src/swap.swaps'),
     },
     modules: [
       config.paths.base('client'),
