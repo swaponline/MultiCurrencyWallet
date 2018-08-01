0f43f66063aedbd2a45005c3322645b0ea63382a
nikdementev <nikita.pietrov.71@mail.ru>
2018-08-01 15:51:53 +0300

    update path to swap.core

--- webpack/common.js
+++ webpack/common.js
@@ -50,7 +50,7 @@ const webpackConfig = {
       config.paths.base('shared'),
       config.paths.base('local_modules'),
       'node_modules',
-      config.paths.swapCore('src/node_modules'),
+      config.paths.swapCore('src/swap.core'),
     ],
     extensions: [ '.js', '.jsx', '.scss' ],
     plugins: [],
