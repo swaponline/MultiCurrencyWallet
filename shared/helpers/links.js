e6e33c06ebd4c7ea10505528b3660abdb1fa8567
Spoki4 <Spokxxl@yandex.ru>
2018-08-20 19:03:51 +0300

    Fix link to testnet in production

--- shared/helpers/links.js
+++ shared/helpers/links.js
@@ -6,7 +6,7 @@ export default {
   feed: '/feed',
   affiliate: '/affiliate',
   listing: '/listing',
-  test: 'https:/testnet.swap.online',
+  test: 'https://testnet.swap.online',
   medium: 'https://medium.com/swaponline',
   twitter: 'https://twitter.com/SwapOnlineTeam',
   facebook: 'https://www.facebook.com/pg/Swaponline-637233326642691',
