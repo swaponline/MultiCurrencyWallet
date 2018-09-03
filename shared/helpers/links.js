eb7324d0a619eb723f49866a94e4db0d67f0864e
Noxon <2914674+noxonsu@users.noreply.github.com>
2018-09-03 08:06:52 +0300

    updatebitcointalk link
    старый ведет на удаленный топик

--- shared/helpers/links.js
+++ shared/helpers/links.js
@@ -15,7 +15,7 @@ export default {
   twitter: 'https://twitter.com/SwapOnlineTeam',
   facebook: 'https://www.facebook.com/pg/Swaponline-637233326642691',
   telegram: 'https://t.me/swaponline',
-  bitcointalk: 'https://bitcointalk.org/index.php?topic=3914826.460',
+  bitcointalk: 'https://bitcointalk.org/index.php?topic=4636633.0',
 
   // footer links
   etherdelta: 'https://etherdelta.com/#0x14a52cf6b4f68431bd5d9524e4fcd6f41ce4ade9-ETH',
