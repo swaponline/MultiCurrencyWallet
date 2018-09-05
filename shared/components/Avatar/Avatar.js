d67b013db2163280750eac7b6e967d3eeb300c6d
Vasilii Zolotukhin <mail@indapublic.com>
2018-09-05 11:17:44 +1000

    * Replace `identicon.js` with `jdenticon`

--- shared/components/Avatar/Avatar.js
+++ shared/components/Avatar/Avatar.js
@@ -1,31 +1,20 @@
 import React from 'react'
-import Identicon from 'identicon.js'
+import jdenticon from 'jdenticon'
 
 import CSSModules from 'react-css-modules'
 import styles from './Avatar.scss'
 
 
-const Avatar = ({ value, className, background, margin, size, format }) => {
-
-  const options = {
-    background,
-    margin,
-    size,
-    format,
-  }
-
-  const data = new Identicon(value, options).toString()
-
-  return (
-    <img className={className} styleName="avatar" alt={value} src={`data:image/svg+xml;base64,${data}`} />
-  )
-}
+const Avatar = ({ value, className, size }) =>
+  <img
+    className={className}
+    styleName="avatar"
+    alt={value}
+    src={`data:image/svg+xml,${encodeURIComponent(jdenticon.toSvg(value, size))}`}
+  />
 
 Avatar.defaultProps = {
-  background: [255, 255, 255, 255],
-  margin: 0,
   size: 35,
-  format: 'svg',
 }
 
 export default CSSModules(Avatar, styles)
