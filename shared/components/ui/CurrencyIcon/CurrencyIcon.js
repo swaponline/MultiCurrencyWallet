d4718f46aa9864ccc53d8666cef959728d38187b
nikdementev <nikita.pietrov.71@mail.ru>
2018-09-11 19:20:59 +0300

    change icons

--- shared/components/ui/CurrencyIcon/CurrencyIcon.js
+++ shared/components/ui/CurrencyIcon/CurrencyIcon.js
@@ -3,42 +3,8 @@ import React from 'react'
 import cssModules from 'react-css-modules'
 import styles from './CurrencyIcon.scss'
 
-import btc from './images/btc.svg'
-import eth from './images/eth.svg'
-import icx from './images/icx.svg'
-import waves from './images/waves.svg'
-import xrp from './images/xrp.svg'
-import bch from './images/bch.svg'
-import nim from './images/nim.svg'
-import eos from './images/eos.svg'
-import swap from './images/swap.svg'
-import usdt from './images/usdt.svg'
-import syc from './images/syc.svg'
-import bee from './images/bee.svg'
-import drt from './images/drt.svg'
-import omg from './images/omg.svg'
-import yup from './images/yup.svg'
-import kn from './images/kn.svg'
+import icons from './images'
 
-
-const icons = {
-  btc,
-  kn,
-  bee,
-  bch,
-  drt,
-  omg,
-  eth,
-  syc,
-  icx,
-  usdt,
-  swap,
-  waves,
-  xrp,
-  eos,
-  yup,
-  nim,
-}
 
 export const iconNames = Object.keys(icons)
 
---
+++ shared/components/ui/CurrencyIcon/images/index.js
@@ -0,0 +1,36 @@
+import btc from './btc.svg'
+import eth from './eth.svg'
+import icx from './icx.svg'
+import waves from './waves.svg'
+import xrp from './xrp.svg'
+import bch from './bch.svg'
+import nim from './nim.svg'
+import eos from './eos.svg'
+import swap from './swap.svg'
+import usdt from './usdt.svg'
+import syc from './syc.svg'
+import bee from './bee.svg'
+import drt from './drt.svg'
+import omg from './omg.svg'
+import yup from './yup.svg'
+import kn from './kn.svg'
+
+
+export default {
+  btc,
+  kn,
+  bee,
+  bch,
+  drt,
+  omg,
+  eth,
+  syc,
+  icx,
+  usdt,
+  swap,
+  waves,
+  xrp,
+  eos,
+  yup,
+  nim,
+}
