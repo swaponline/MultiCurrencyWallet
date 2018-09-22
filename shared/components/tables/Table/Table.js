67fb6e89b752c572fdd8d2de9b5bd0468cb114e2
Ilya <chernikov.i123@gmail.com>
2018-09-22 18:47:09 +1000

    Fix eslint errors in Table.js

--- shared/components/tables/Table/Table.js
+++ shared/components/tables/Table/Table.js
@@ -5,42 +5,36 @@ import styles from './Table.scss'
 
 @CSSModules(styles, { allowMultiple: true })
 export default class Table extends React.Component {
-
   constructor() {
     super()
 
     this.state = {
-      sticky: false
+      sticky: false,
     }
-
   }
 
   componentDidMount() {
-    window.addEventListener('scroll', this.handleScrollTable);
+    window.addEventListener('scroll', this.handleScrollTable)
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScrollTable);
+    window.removeEventListener('scroll', this.handleScrollTable)
   }
 
   handleScrollTable = () => {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-    let table = document.querySelector('table').offsetTop;
-      if ( scrollTop > table ) {
-        this.setState(() => ({sticky: true}))
-      }
-      else {
-        this.setState(() => ({sticky: false}))
-      }
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop
+    let table = document.querySelector('table').offsetTop
+    if (scrollTop > table) {
+      this.setState(() => ({ sticky: true }))
+    } else {
+      this.setState(() => ({ sticky: false }))
+    }
   }
 
   shouldComponentUpdate(nextProps, nextState) {
     const { rows, isLoading } = this.props
-    return (
-      isLoading !== nextProps.isLoading || rows !== nextProps.rows || this.state.sticky !== nextState.sticky
-    )
+    return isLoading !== nextProps.isLoading || rows !== nextProps.rows || this.state.sticky !== nextState.sticky
   }
-
 
   render() {
     const { titles, rows, rowRender, textIfEmpty, isLoading, loadingText, classTitle } = this.props
@@ -50,39 +44,29 @@ export default class Table extends React.Component {
       <table styleName={sticky ? 'table table-fixed' : 'table'} className={classTitle}>
         <thead>
           <tr>
-            {
-              titles.filter(title => !!title).map((title, index) => (
-                <th key={index}>{title}</th>
-              ))
-            }
+            {titles.filter(title => !!title).map((title, index) => (
+              <th key={index}>{title}</th>
+            ))}
           </tr>
         </thead>
         <tbody>
-          {
-            isLoading && (
-              <tr>
-                <td styleName="color">{loadingText}</td>
-              </tr>
-            )
-          }
-          {
-            !isLoading && !rows.length && (
-              <tr>
-                <td styleName="color">{textIfEmpty}</td>
-              </tr>
-            )
-          }
-          {
-            !isLoading && !!rows.length && rows.map((row, rowIndex) => (
-              typeof rowRender === 'function' && rowRender(row, rowIndex)
-            ))
-          }
+          {isLoading && (
+            <tr>
+              <td styleName="color">{loadingText}</td>
+            </tr>
+          )}
+          {!isLoading &&
+            !rows.length && (
+            <tr>
+              <td styleName="color">{textIfEmpty}</td>
+            </tr>
+          )}
+          {!isLoading && !!rows.length && rows.map((row, rowIndex) => typeof rowRender === 'function' && rowRender(row, rowIndex))}
         </tbody>
       </table>
     )
   }
 }
-
 
 Table.defaultProps = {
   textIfEmpty: 'The table is empty',
