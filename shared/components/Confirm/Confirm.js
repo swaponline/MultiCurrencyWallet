8366166a1ccceeaac76caf8b0412d3e0abc4b962
Ilya <chernikov.i123@gmail.com>
2018-09-26 12:26:26 +1000

    Make Confirm component centered by default

--- shared/components/Confirm/Confirm.js
+++ shared/components/Confirm/Confirm.js
@@ -5,18 +5,21 @@ import styles from './Confirm.scss'
 
 import SubTitle from 'components/PageHeadline/SubTitle/SubTitle'
 import Button from 'components/controls/Button/Button'
+import Center from 'components/layout/Center/Center'
 
 
 const Confirm = ({ rootClassName, isConfirm, isReject, title, animation }) => (
-  <div styleName={animation ? 'confirm animation' : 'confirm'} className={rootClassName}>
-    <SubTitle>
-      {title}
-    </SubTitle>
-    <div styleName="row" >
-      <Button brand onClick={isConfirm}>Yes</Button>
-      <Button brand onClick={isReject}>No</Button>
+  <Center>
+    <div styleName={animation ? 'confirm animation' : 'confirm'} className={rootClassName}>
+      <SubTitle>
+        {title}
+      </SubTitle>
+      <div styleName="row" >
+        <Button brand onClick={isConfirm}>Yes</Button>
+        <Button brand onClick={isReject}>No</Button>
+      </div>
     </div>
-  </div>
+  </Center>
 )
 
 export default cssModules(Confirm, styles, { allowMultiple: true })
