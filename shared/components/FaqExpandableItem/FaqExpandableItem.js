b8846e319e5aed49e326da599bf11e7255fb81b1
Ilya <chernikov.i123@gmail.com>
2018-10-02 19:02:51 +1000

    Replace fetch of the faq with hardcoded links on wiki

--- shared/components/FaqExpandableItem/FaqExpandableItem.js
+++ shared/components/FaqExpandableItem/FaqExpandableItem.js
@@ -1,45 +1,49 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import cssModules from 'react-css-modules'
-import cx from 'classnames'
 
 import styles from './FaqExpandableItem.scss'
 
 
-@cssModules(styles, { allowMultiple: true })
-export class FaqExpandableItem extends React.Component {
+@cssModules(styles)
+export class FaqExpandableItem extends React.PureComponent {
 
   static propTypes = {
     question: PropTypes.string.isRequired,
-    answer: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    // answer: PropTypes.string.isRequired,
   }
 
-  state = {
-    expanded: false,
-  }
+  // state = {
+  //   expanded: false,
+  // }
 
-  toggle = () => {
-    this.setState({ expanded: !this.state.expanded })
-  }
+  // toggle = () => {
+  //   this.setState({ expanded: !this.state.expanded })
+  // }
 
   render() {
-    const { answer, question } = this.props
-    const { expanded } = this.state
+    const { question } = this.props
+    // const { expanded } = this.state
 
-    const styleName = cx('container', {
-      'active': expanded,
-    })
+    // const styleName = cx('container', {
+    //   'active': expanded,
+    // })
 
     return (
-      <div styleName={styleName}>
-        <div styleName="header" onClick={this.toggle}>
-          <div dangerouslySetInnerHTML={{ __html: question }} />
-          <span className={`fas fa-${expanded ? 'minus' : 'plus'}`} styleName="icon" />
+      <div styleName="container">
+        <div styleName="header" onClick={this.goToLink}>
+          {question}
+          {/* <span className={`fas fa-${expanded ? 'minus' : 'plus'}`} styleName="icon" /> */}
         </div>
-        {
-          expanded && <div styleName="description" dangerouslySetInnerHTML={{ __html: answer }} />
-        }
+        {/* { */}
+        {/* expanded && <div styleName="description" dangerouslySetInnerHTML={{ __html: answer }} /> */}
+        {/* } */}
       </div>
     )
+  }
+
+  goToLink = () => {
+    window.open(this.props.link)
   }
 }
