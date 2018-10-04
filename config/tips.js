ea2916f8b6662533523bf1d2817c8c51196f595b
Noxon <2914674+noxonsu@users.noreply.github.com>
2018-10-04 13:11:29 +0300

    отзыв вера

--- config/tips.js
+++ config/tips.js
@@ -2,7 +2,7 @@ const tips = {
   loader: [
     'Do not forget to save your private keys!',
     '"Wow, this is awesome!" @dexx7y Maintainer of Omni Core about our USDT swaps',
-    '"That is great news" Roger Ver CEO of bitcoin.com about connect ИСР to swap.online'
+    '"That is great news" Roger Ver CEO of bitcoin.com about connect Bitcoin Cash to swap.online'
   ],
 }
 
