fb26016804d2fb003ef1c396dbe08964191a6501
Aleksey Bykhun <theisease@gmail.com>
2018-10-04 13:23:32 +0300

    Update tips.js

--- config/tips.js
+++ config/tips.js
@@ -1,8 +1,8 @@
 const tips = {
   loader: [
     'Do not forget to save your private keys!',
-    '"Wow, this is awesome!" @dexx7y Maintainer of Omni Core about our USDT swaps',
-    '"That is great news" Roger Ver CEO of bitcoin.com about connect Bitcoin Cash to swap.online'
+    '"Wow, this is awesome!" – @dexx7y Maintainer of Omni Core about our USDT swaps',
+    '"That is great news" – Roger Ver, CEO of bitcoin.com, about connecting Bitcoin Cash to swap.online'
   ],
 }
 
