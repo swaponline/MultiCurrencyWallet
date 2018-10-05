52aef0fa11467ca125d781cf20399c81b5223f10
nikdementev <nikita.pietrov.71@mail.ru>
2018-10-05 15:14:30 +0300

    add tokens

--- shared/helpers/utils.js
+++ shared/helpers/utils.js
@@ -1,57 +1,7 @@
 /* eslint-disable */
-import path  from 'path'
-import fs    from 'fs'
+import path from 'path'
+import config from 'app-config'
 
-
-export const removeRepo = (pathToRepo) => {
-  /**
-   * Check NODE_ENV if env = test or window === undefined
-   * return this function
-   * else delete REPO directory
-   */
-  if (
-    typeof window !== 'undefined' ||
-    !fs.existsSync(pathToRepo)
-  ) {
-    console.log('ENV === test or start with browser or file not exists with path')
-    return
-  }
-
-  try {
-    /**
-     * check files in directory
-     * and call functions for each of them
-     */
-    for (let file of fs.readdirSync(pathToRepo)) {
-      /**
-       * Create path to file inner directory
-       */
-      const curPath = path.join(pathToRepo, file)
-
-      /**
-       * if file or directory not exist
-       * then missing this path
-       */
-      if (!fs.existsSync(curPath)) {
-        continue
-      }
-
-      /**
-       * if check path on directory then
-       * recursive call else delete file
-       */
-      (fs.lstatSync(curPath).isDirectory())
-        ? removeRepo(curPath)
-        : fs.unlinkSync(curPath)
-    }
-
-    /** If not files then remove directory */
-    fs.rmdirSync(pathToRepo)
-  } catch (err) {
-    console.error(err)
-    process.exit(1)
-  }
-}
 
 export const exitListener = () => {
   /**
@@ -70,9 +20,8 @@ export const exitListener = () => {
 
 export const capitalize = s => s.charAt(0).toUpperCase() + s.substr(1)
 
-
-export const createRepo = (dirpath = './data/') => {
-  dirpath += Math.ceil(Math.random() * 10000)
+export const createRepo = (dirpath = `./data/`) => {
+  // dirpath += Math.ceil(Math.random() * 10000)
   return path.resolve(dirpath)
 }
 
