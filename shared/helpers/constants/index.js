86500db36ab9ca6161ecfe0efd3db9e8db6303e9
nikdementev <nikita.pietrov.71@mail.ru>
2018-10-14 10:15:53 +0300

    add export default constants

--- shared/helpers/constants/index.js
+++ shared/helpers/constants/index.js
@@ -2,11 +2,17 @@ import localStorage from './localStorage'
 import privateKeyNames from './privateKeyNames'
 import notifications from './notifications'
 import modals from './modals'
+import tradeTicker from './TRADE_TICKERS'
+import tokenDecimals from './TOKEN_DECIMALS'
+import pairTypes from './PAIR_TYPES'
 
 
 export default {
   localStorage,
+  tradeTicker,
   privateKeyNames,
+  tokenDecimals,
   notifications,
+  pairTypes,
   modals,
 }
