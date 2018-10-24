283446a6851038eab9997abce6960be94e2099d2
Pasha8914 <uhau-buhau@mail.ru>
2018-10-25 09:45:10 +1000

    erc 20 full name

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -2,81 +2,101 @@ export default {
   swap: {
     address: '0x14a52cf6B4F68431bd5D9524E4fcD6F41ce4ADe9',
     decimals: 18,
+    fullName: 'swap',
   },
   lev: {
     address: '0x0f4ca92660efad97a9a70cb0fe969c755439772c',
     decimals: 9,
+    fullName: 'Leverj',
   },
   drt: {
     address: '0x9af4f26941677c706cfecf6d3379ff01bb85d5ab',
     decimals: 8,
+    fullName: 'DomRaider',
   },
   syc2: {
     address: '0x49feeF410293261c04F1d14600Ba427F8eED8723',
     decimals: 2,
+    fullName: 'Synexcoin2',
   },
   arn: {
     address: '0xBA5F11b16B155792Cf3B2E6880E8706859A8AEB6',
     decimals: 8,
+    fullName: 'Aeron',
   },
   gusd: {
     address: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd',
     decimals: 2,
+    fullName: 'Gemini dollar',
   },
   kn: {
     address: '0xbfd18F20423694a69e35d65cB9c9D74396CC2c2d',
     decimals: 18,
+    fullName: 'Know',
   },
   yup: {
     address: '0xD9A12Cde03a86E800496469858De8581D3A5353d',
     decimals: 18,
+    fullName: 'Yupie',
   },
   jot: {
     address: '0xdb455c71c1bc2de4e80ca451184041ef32054001',
     decimals: 18,
+    fullName: 'Jury',
   },
   noxon: {
     address: '0x9E4AD79049282F942c1b4c9b418F0357A0637017',
     decimals: 0,
+    fullName: 'noxon',
   },
   bxb: {
     address: '0x60cbB964Db3f60D14f0d00B5109BccFB01981Aca',
     decimals: 18,
+    fullName: 'bitXbit',
   },
   dcn: {
     address: '0x08d32b0da63e2C3bcF8019c9c5d849d7a9d791e6',
     decimals: 0,
+    fullName: 'Dentacoin',
   },
   mbrs: {
     address: '0x386467F1f3ddbE832448650418311a479EECFC57',
     decimals: 0,
+    fullName: 'Embers',
   },
   sct: {
     address: '0x63b992e6246d88f07fc35a056d2c365e6d441a3d',
     decimals: 18,
+    fullName: 'Soma',
   },
   nexo: {
     address: '0xb62132e35a6c13ee1ee0f84dc5d40bad8d815206',
     decimals: 18,
+    fullName: 'nexo',
   },
   btrm: {
     address: '0xae72146eb535607ee79f5d8834303ea18751845f',
     decimals: 18,
+    fullName: 'Betrium',
   },
   bnty: {
     address: '0xd2d6158683aee4cc838067727209a0aaf4359de3',
     decimals: 18,
+    fullName: 'Bounty0x',
   },
   pbl: {
     address: '0x55648de19836338549130b1af587f16bea46f66b',
     decimals: 18,
+    fullName: 'Publica',
   },
   senc: {
     address: '0xa13f0743951b4f6e3e3aa039f682e17279f52bc3',
     decimals: 18,
+    fullName: 'Sentinel Chain',
   },
   mth: {
     address: '0xaf4dce16da2877f8c9e00544c93b62ac40631f16',
     decimals: 5,
+    fullName: 'Meritage',
   },
 }
