06bdae8771d99fa642e6da030b8819b87f7a0503
ED <ev@aisapr.ru>
2018-10-25 01:15:22 +1000

    Restore bitpay testnet link;

--- config/testnet/link.js
+++ config/testnet/link.js
@@ -1,5 +1,5 @@
 export default {
-  bitpay: 'https://test-insight.bitpay.com',
+  bitpay: 'https://test-insight.swap.online/insight',
   etherscan: 'https://rinkeby.etherscan.io',
   eos: 'https://eospark.com/Jungle',
   tlos: 'http://testnet.telosfoundation.io',
