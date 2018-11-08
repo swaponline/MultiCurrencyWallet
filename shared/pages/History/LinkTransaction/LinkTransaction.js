8040b2585778401c2a8f3a1ddfe953463a993ed5
Pasha8914 <uhau-buhau@mail.ru>
2018-11-09 03:56:30 +1000

    erc20

--- shared/pages/History/LinkTransaction/LinkTransaction.js
+++ shared/pages/History/LinkTransaction/LinkTransaction.js
@@ -4,15 +4,19 @@ import config from 'app-config'
 import Href from 'components/Href/Href'
 
 
+const erc20Tokens = () => {Object.keys(config.erc20)
+  .map(key => {
+    type.toLowerCase() === `${key}` && <Href tab={`${config.link.etherscan}/tx/${hash}`} >{children}</Href>
+  })
+}
+
 const LinkTransaction = ({ type, children, hash }) => (
   <Fragment>
     { type.toLowerCase() === 'eth' && <Href tab={`${config.link.etherscan}/tx/${hash}`} >{children}</Href> }
     { type.toLowerCase() === 'btc' && <Href tab={`${config.link.bitpay}/tx/${hash}`} >{children}</Href> }
     { type.toLowerCase() === 'ltc' && <Href tab={`${config.link.ltc}/tx/${hash}`} >{children}</Href> }
-    { type.toLowerCase() === 'noxon' && <Href tab={`${config.link.etherscan}/tx/${hash}`} >{children}</Href> }
-    { type.toLowerCase() === 'swap' && <Href tab={`${config.link.etherscan}/tx/${hash}`} >{children}</Href> }
+    { erc20Tokens }
   </Fragment>
 )
-
 
 export default LinkTransaction
