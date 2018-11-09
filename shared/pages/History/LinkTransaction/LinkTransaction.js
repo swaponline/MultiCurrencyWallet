ed53b04433f9d452b677b3f1faffc7b564a527e1
Pasha8914 <uhau-buhau@mail.ru>
2018-11-09 22:35:19 +1000

    coment change

--- shared/pages/History/LinkTransaction/LinkTransaction.js
+++ shared/pages/History/LinkTransaction/LinkTransaction.js
@@ -3,17 +3,12 @@ import config from 'app-config'
 import Href from 'components/Href/Href'
 
 
-const erc20Link = (type, hash, children) => Object.keys(config.erc20)
-  .map(key => type.toLowerCase() === key
-    && <Href tab={`${config.link.etherscan}/tx/${hash}`}>{children}</Href>
-  )
-
 const LinkTransaction = ({ type, children, hash }) => (
   <Fragment>
     { type.toLowerCase() === 'eth' && <Href tab={`${config.link.etherscan}/tx/${hash}`} >{children}</Href> }
     { type.toLowerCase() === 'btc' && <Href tab={`${config.link.bitpay}/tx/${hash}`} >{children}</Href> }
     { type.toLowerCase() === 'ltc' && <Href tab={`${config.link.ltc}/tx/${hash}`} >{children}</Href> }
-    { erc20Link(type, hash, children) }
+    { Object.keys(config.erc20).includes(type.toLowerCase()) && <Href tab={`${config.link.etherscan}/tx/${hash}`}>{children}</Href>}
   </Fragment>
 )
 
