be74cfa73d9d8fceda84339e80eb71ba37e6b5a6
Pasha8914 <uhau-buhau@mail.ru>
2018-11-09 20:45:14 +1000

    discord

--- shared/helpers/links.js
+++ shared/helpers/links.js
@@ -18,6 +18,7 @@ export default {
   facebook: 'https://www.facebook.com/pg/Swaponline-637233326642691',
   telegram: 'https://t.me/swaponline',
   bitcointalk: 'https://bitcointalk.org/index.php?topic=4636633.0',
+  discord: 'https://discordapp.com/invite/juG7UZH',
 
   // footer links
   etherdelta: 'https://etherdelta.com/#0x14a52cf6b4f68431bd5d9524e4fcd6f41ce4ade9-ETH',
