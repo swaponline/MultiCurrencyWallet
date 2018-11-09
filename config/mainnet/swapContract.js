b44fc626d1f42ecbfcf8474a6e3d9ed5d200da41
Aleksey Bykhun <theisease@gmail.com>
2018-11-09 16:51:21 +0300

    fix: update mainnet contract for ERC20 tokens

--- config/mainnet/swapContract.js
+++ config/mainnet/swapContract.js
@@ -1,5 +1,5 @@
 export default {
-  erc20: '0x85F806b0df30709886C22ed1be338d2c647Abd6B',
+  erc20: '0x30f85eBCFAc9f6Dd69503955e95ab7F55Fa3fDeC',
   eth: '0x843FcaAeb0Cce5FFaf272F5F2ddFFf3603F9c2A0',
   eos: 'swaponline42',
 }
