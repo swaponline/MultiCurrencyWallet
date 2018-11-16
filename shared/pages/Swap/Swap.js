5f7df926fd5172b42bb9d7767b10222cd4654de0
ED <ev@aisapr.ru>
2018-11-16 10:57:36 +1000

    Add address and address link for swap, then it's not enough money for swap.

--- shared/pages/Swap/Swap.js
+++ shared/pages/Swap/Swap.js
@@ -11,18 +11,24 @@ import Share from './Share/Share'
 import EmergencySave from './EmergencySave/EmergencySave'
 
 
-@connect({
+@connect(({
+  user: { ethData, btcData, bchData, tokensData, eosData, telosData, nimData, usdtData, ltcData },
+}) => ({
+  items: [ ethData, btcData, eosData, telosData, bchData, ltcData, usdtData /* nimData */ ],
+  tokenItems: [ ...Object.keys(tokensData).map(k => (tokensData[k])) ],
   errors: 'api.errors',
   checked: 'api.checked',
-})
+}))
 export default class SwapComponent extends PureComponent {
 
   state = {
     swap: null,
     SwapComponent: null,
+    currencyData: null,
   }
 
   componentWillMount() {
+    const { items, tokenItems } = this.props
     let { match : { params : { orderId } }, history } = this.props
 
     if (!orderId) {
@@ -33,12 +39,15 @@ export default class SwapComponent extends PureComponent {
     try {
       const swap = new Swap(orderId)
       const SwapComponent = swapComponents[swap.flow._flowName]
+      const currencyData = items.concat(tokenItems)
+        .filter(item => item.currency === swap.sellCurrency.toUpperCase())[0]
 
       window.swap = swap
 
       this.setState({
         SwapComponent,
         swap,
+        currencyData,
       })
 
     } catch (error) {
@@ -71,7 +80,7 @@ export default class SwapComponent extends PureComponent {
   }
 
   render() {
-    const { swap, SwapComponent } = this.state
+    const { swap, SwapComponent, currencyData } = this.state
 
     if (!swap || !SwapComponent) {
       return null
@@ -79,7 +88,7 @@ export default class SwapComponent extends PureComponent {
 
     return (
       <div style={{ paddingLeft: '30px', paddingTop: '30px' }}>
-        <SwapComponent swap={swap} >
+        <SwapComponent swap={swap} currencyData={currencyData}>
           <Share flow={swap.flow} />
           <EmergencySave flow={swap.flow} />
         </SwapComponent>
