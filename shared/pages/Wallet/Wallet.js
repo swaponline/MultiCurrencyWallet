7c9c125be7506cde8380af11dcc46cbb5015adee
Noxon <2914674+noxonsu@users.noreply.github.com>
2018-11-23 03:57:53 +0300

    Update Wallet.js

--- shared/pages/Wallet/Wallet.js
+++ shared/pages/Wallet/Wallet.js
@@ -125,7 +125,7 @@ export default class Wallet extends Component {
           <SubTitle>
             <FormattedMessage id="3333Wallet104" defaultMessage="Your online cryptocurrency wallet" />
           </SubTitle>
-          Deposit funds to addresses below
+          Depositуу4 funds to addresses below
         </PageHeadline>
         <Table
           id="table-wallet"
