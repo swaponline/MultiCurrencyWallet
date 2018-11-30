bc8348af9d27bd0bea8ea75b2b2136ccc1dee9cb
Pasha8914 <uhau-buhau@mail.ru>
2018-12-01 01:57:39 +1000

    фикс по феншую

--- shared/pages/Wallet/Wallet.js
+++ shared/pages/Wallet/Wallet.js
@@ -104,7 +104,7 @@ export default class Wallet extends Component {
     return (
       <section styleName={isMobile ? 'sectionWalletMobile' : 'sectionWallet'}>
         { showSaveKeysModal && <SaveKeysModal /> }
-        <PageHeadline style={{ marginLeft: '10px', height: '130px' }}>
+        <PageHeadline styleName="pageLine">
           <SubTitle>
             <FormattedMessage id="Wallet104" defaultMessage="Your online cryptocurrency wallet" />
           </SubTitle>
