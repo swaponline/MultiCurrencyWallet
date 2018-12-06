ba510b6e787886dcd2349dec94edf7db72016c4d
nikdementev <nikita.pietrov.71@mail.ru>
2018-12-06 17:10:48 +0300

    delete comment

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -3,210 +3,210 @@ export default {
     address: '0x14a52cf6B4F68431bd5D9524E4fcD6F41ce4ADe9',
     decimals: 18,
     fullName: 'swap',
+  },lev: {
+    address: '0x0f4ca92660efad97a9a70cb0fe969c755439772c',
+    decimals: 9,
+    fullName: 'Leverj',
   },
-  // lev: {
-  //   address: '0x0f4ca92660efad97a9a70cb0fe969c755439772c',
-  //   decimals: 9,
-  //   fullName: 'Leverj',
-  // },
-  // drt: {
-  //   address: '0x9af4f26941677c706cfecf6d3379ff01bb85d5ab',
-  //   decimals: 8,
-  //   fullName: 'DomRaider',
-  // },
-  // syc2: {
-  //   address: '0x49feeF410293261c04F1d14600Ba427F8eED8723',
-  //   decimals: 2,
-  //   fullName: 'Synexcoin2',
-  // },
-  // arn: {
-  //   address: '0xBA5F11b16B155792Cf3B2E6880E8706859A8AEB6',
-  //   decimals: 8,
-  //   fullName: 'Aeron',
-  // },
-  // deb: {
-  //   address: '0x151202c9c18e495656f372281f493eb7698961d5',
-  //   decimals: 18,
-  //   fullName: 'Debitum',
-  // },
-  // gusd: {
-  //   address: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd',
-  //   decimals: 2,
-  //   fullName: 'Gemini dollar',
-  // },
-  // kn: {
-  //   address: '0xbfd18F20423694a69e35d65cB9c9D74396CC2c2d',
-  //   decimals: 18,
-  //   fullName: 'Know',
-  // },
-  // yup: {
-  //   address: '0xD9A12Cde03a86E800496469858De8581D3A5353d',
-  //   decimals: 18,
-  //   fullName: 'Yupie',
-  // },
-  // jot: {
-  //   address: '0xdb455c71c1bc2de4e80ca451184041ef32054001',
-  //   decimals: 18,
-  //   fullName: 'Jury',
-  // },
-  // noxon: {
-  //   address: '0x9E4AD79049282F942c1b4c9b418F0357A0637017',
-  //   decimals: 0,
-  //   fullName: 'noxon',
-  // },
-  // bxb: {
-  //   address: '0x60cbB964Db3f60D14f0d00B5109BccFB01981Aca',
-  //   decimals: 18,
-  //   fullName: 'Bobcoin',
-  // },
-  // dcn: {
-  //   address: '0x08d32b0da63e2C3bcF8019c9c5d849d7a9d791e6',
-  //   decimals: 0,
-  //   fullName: 'Dentacoin',
-  // },
-  // mbrs: {
-  //   address: '0x386467F1f3ddbE832448650418311a479EECFC57',
-  //   decimals: 0,
-  //   fullName: 'Embers',
-  // },
-  // sct: {
-  //   address: '0x63b992e6246d88f07fc35a056d2c365e6d441a3d',
-  //   decimals: 18,
-  //   fullName: 'Soma',
-  // },
-  // nexo: {
-  //   address: '0xb62132e35a6c13ee1ee0f84dc5d40bad8d815206',
-  //   decimals: 18,
-  //   fullName: 'nexo',
-  // },
-  // btrm: {
-  //   address: '0xae72146eb535607ee79f5d8834303ea18751845f',
-  //   decimals: 18,
-  //   fullName: 'Betrium',
-  // },
-  // bnty: {
-  //   address: '0xd2d6158683aee4cc838067727209a0aaf4359de3',
-  //   decimals: 18,
-  //   fullName: 'Bounty0x',
-  // },
-  // pbl: {
-  //   address: '0x55648de19836338549130b1af587f16bea46f66b',
-  //   decimals: 18,
-  //   fullName: 'Publica',
-  // },
-  // senc: {
-  //   address: '0xa13f0743951b4f6e3e3aa039f682e17279f52bc3',
-  //   decimals: 18,
-  //   fullName: 'Sentinel Chain',
-  // },
-  // mth: {
-  //   address: '0xaf4dce16da2877f8c9e00544c93b62ac40631f16',
-  //   decimals: 5,
-  //   fullName: 'Meritage',
-  // },
-  // star: {
-  //   address: '0xf70a642bd387f94380ffb90451c2c81d4eb82cbc',
-  //   decimals: 18,
-  //   fullName: 'Starbase',
-  // },
-  // xyo: {
-  //   address: '0x55296f69f40ea6d20e478533c15a6b08b654e758',
-  //   decimals: 18,
-  //   fullName: 'XY Oracle',
-  // },
-  // xnk: {
-  //   address: '0xbc86727e770de68b1060c91f6bb6945c73e10388',
-  //   decimals: 18,
-  //   fullName: 'Link Protocol',
-  // },
-  // loc: {
-  //   address: '0x5e3346444010135322268a4630d2ed5f8d09446c',
-  //   decimals: 18,
-  //   fullName: 'LockTrip',
-  // },
-  // key: {
-  //   address: '0x4cc19356f2d37338b9802aa8e8fc58b0373296e7',
-  //   decimals: 18,
-  //   fullName: 'SelfKey',
-  // },
-  // dai: {
-  //   address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
-  //   decimals: 18,
-  //   fullName: 'Dai StableCoin',
-  // },
-  // tnt: {
-  //   address: '0x08f5a9235b08173b7569f83645d2c7fb55e8ccd8',
-  //   decimals: 8,
-  //   fullName: 'Tierion Network',
-  // },
-  // avt: {
-  //   address: '0x0d88ed6e74bbfd96b831231638b66c05571e824f',
-  //   decimals: 18,
-  //   fullName: 'Aventus Protocol',
-  // },
-  // hbt: {
-  //   address: '0xdd6c68bb32462e01705011a4e2ad1a60740f217f',
-  //   decimals: 15,
-  //   fullName: 'Hubiits',
-  // },
-  // view: {
-  //   address: '0xf03f8d65bafa598611c3495124093c56e8f638f0',
-  //   decimals: 18,
-  //   fullName: 'View',
-  // },
-  // cs: {
-  //   address: '0x46b9ad944d1059450da1163511069c718f699d31',
-  //   decimals: 18,
-  //   fullName: 'Credits',
-  // },
-  // pix: {
-  //   address: '0x8eFFd494eB698cc399AF6231fCcd39E08fd20B15',
-  //   decimals: 0,
-  //   fullName: 'PIX Token',
-  // },
-  // gen: {
-  //   address: '0x543ff227f64aa17ea132bf9886cab5db55dcaddf',
-  //   decimals: 18,
-  //   fullName: 'DAO Stack',
-  // },
-  // data: {
-  //   address: '0x0cf0ee63788a0849fe5297f3407f701e122cc023',
-  //   decimals: 18,
-  //   fullName: 'Streamr DATAcoin',
-  // },
-  // gai: {
-  //   address: '0xf207ccfe6d1b90b0b7817ce2d5d262af1514e37b',
-  //   decimals: 18,
-  //   fullName: 'GraphGrailAi Token',
-  // },
-  // emtv: {
-  //   address: '0x07a7ed332c595b53a317afcee50733af571475e7',
-  //   decimals: 18,
-  //   fullName: 'Multiversum',
-  // },
-  // cgc: {
-  //   address: '0x2d9765a94ff22e0ca3afc3e3f4b116de2b67582a',
-  //   decimals: 16,
-  //   fullName: 'CGCOINS',
-  // },
-  // vite: {
-  //   address: '0x1b793E49237758dBD8b752AFC9Eb4b329d5Da016',
-  //   decimals: 18,
-  //   fullName: 'ViteToken',
-  // },
-  // mot: {
-  //   address: '0x263c618480dbe35c300d8d5ecda19bbb986acaed',
-  //   decimals: 18,
-  //   fullName: 'MOT',
-  // },
-  // dov: {
-  //   address: '0xac3211a5025414Af2866FF09c23FC18bc97e79b1',
-  //   decimals: 18,
-  //   fullName: 'DOVU',
-  // },
-  // xbx: {
-  //   address: '0x2c745664FB4e9528fB32487D834e4aACffF1F027',
-  //   decimals: 18,
-  //   fullName: 'Bitex Coin',
-  // },
+  drt: {
+    address: '0x9af4f26941677c706cfecf6d3379ff01bb85d5ab',
+    decimals: 8,
+    fullName: 'DomRaider',
+  },
+  syc2: {
+    address: '0x49feeF410293261c04F1d14600Ba427F8eED8723',
+    decimals: 2,
+    fullName: 'Synexcoin2',
+  },
+  arn: {
+    address: '0xBA5F11b16B155792Cf3B2E6880E8706859A8AEB6',
+    decimals: 8,
+    fullName: 'Aeron',
+  },
+  deb: {
+    address: '0x151202c9c18e495656f372281f493eb7698961d5',
+    decimals: 18,
+    fullName: 'Debitum',
+  },
+  gusd: {
+    address: '0x056fd409e1d7a124bd7017459dfea2f387b6d5cd',
+    decimals: 2,
+    fullName: 'Gemini dollar',
+  },
+  kn: {
+    address: '0xbfd18F20423694a69e35d65cB9c9D74396CC2c2d',
+    decimals: 18,
+    fullName: 'Know',
+  },
+  yup: {
+    address: '0xD9A12Cde03a86E800496469858De8581D3A5353d',
+    decimals: 18,
+    fullName: 'Yupie',
+  },
+  jot: {
+    address: '0xdb455c71c1bc2de4e80ca451184041ef32054001',
+    decimals: 18,
+    fullName: 'Jury',
+  },
+  noxon: {
+    address: '0x9E4AD79049282F942c1b4c9b418F0357A0637017',
+    decimals: 0,
+    fullName: 'noxon',
+  },
+  bxb: {
+    address: '0x60cbB964Db3f60D14f0d00B5109BccFB01981Aca',
+    decimals: 18,
+    fullName: 'Bobcoin',
+  },
+  dcn: {
+    address: '0x08d32b0da63e2C3bcF8019c9c5d849d7a9d791e6',
+    decimals: 0,
+    fullName: 'Dentacoin',
+  },
+  mbrs: {
+    address: '0x386467F1f3ddbE832448650418311a479EECFC57',
+    decimals: 0,
+    fullName: 'Embers',
+  },
+  sct: {
+    address: '0x63b992e6246d88f07fc35a056d2c365e6d441a3d',
+    decimals: 18,
+    fullName: 'Soma',
+  },
+  nexo: {
+    address: '0xb62132e35a6c13ee1ee0f84dc5d40bad8d815206',
+    decimals: 18,
+    fullName: 'nexo',
+  },
+  btrm: {
+    address: '0xae72146eb535607ee79f5d8834303ea18751845f',
+    decimals: 18,
+    fullName: 'Betrium',
+  },
+  bnty: {
+    address: '0xd2d6158683aee4cc838067727209a0aaf4359de3',
+    decimals: 18,
+    fullName: 'Bounty0x',
+  },
+  pbl: {
+    address: '0x55648de19836338549130b1af587f16bea46f66b',
+    decimals: 18,
+    fullName: 'Publica',
+  },
+  senc: {
+    address: '0xa13f0743951b4f6e3e3aa039f682e17279f52bc3',
+    decimals: 18,
+    fullName: 'Sentinel Chain',
+  },
+  mth: {
+    address: '0xaf4dce16da2877f8c9e00544c93b62ac40631f16',
+    decimals: 5,
+    fullName: 'Meritage',
+  },
+  star: {
+    address: '0xf70a642bd387f94380ffb90451c2c81d4eb82cbc',
+    decimals: 18,
+    fullName: 'Starbase',
+  },
+  xyo: {
+    address: '0x55296f69f40ea6d20e478533c15a6b08b654e758',
+    decimals: 18,
+    fullName: 'XY Oracle',
+  },
+  xnk: {
+    address: '0xbc86727e770de68b1060c91f6bb6945c73e10388',
+    decimals: 18,
+    fullName: 'Link Protocol',
+  },
+  loc: {
+    address: '0x5e3346444010135322268a4630d2ed5f8d09446c',
+    decimals: 18,
+    fullName: 'LockTrip',
+  },
+  key: {
+    address: '0x4cc19356f2d37338b9802aa8e8fc58b0373296e7',
+    decimals: 18,
+    fullName: 'SelfKey',
+  },
+  dai: {
+    address: '0x89d24a6b4ccb1b6faa2625fe562bdd9a23260359',
+    decimals: 18,
+    fullName: 'Dai StableCoin',
+  },
+  tnt: {
+    address: '0x08f5a9235b08173b7569f83645d2c7fb55e8ccd8',
+    decimals: 8,
+    fullName: 'Tierion Network',
+  },
+  avt: {
+    address: '0x0d88ed6e74bbfd96b831231638b66c05571e824f',
+    decimals: 18,
+    fullName: 'Aventus Protocol',
+  },
+  hbt: {
+    address: '0xdd6c68bb32462e01705011a4e2ad1a60740f217f',
+    decimals: 15,
+    fullName: 'Hubiits',
+  },
+  view: {
+    address: '0xf03f8d65bafa598611c3495124093c56e8f638f0',
+    decimals: 18,
+    fullName: 'View',
+  },
+  cs: {
+    address: '0x46b9ad944d1059450da1163511069c718f699d31',
+    decimals: 18,
+    fullName: 'Credits',
+  },
+  pix: {
+    address: '0x8eFFd494eB698cc399AF6231fCcd39E08fd20B15',
+    decimals: 0,
+    fullName: 'PIX Token',
+  },
+  gen: {
+    address: '0x543ff227f64aa17ea132bf9886cab5db55dcaddf',
+    decimals: 18,
+    fullName: 'DAO Stack',
+  },
+  data: {
+    address: '0x0cf0ee63788a0849fe5297f3407f701e122cc023',
+    decimals: 18,
+    fullName: 'Streamr DATAcoin',
+  },
+  gai: {
+    address: '0xf207ccfe6d1b90b0b7817ce2d5d262af1514e37b',
+    decimals: 18,
+    fullName: 'GraphGrailAi Token',
+  },
+  emtv: {
+    address: '0x07a7ed332c595b53a317afcee50733af571475e7',
+    decimals: 18,
+    fullName: 'Multiversum',
+  },
+  cgc: {
+    address: '0x2d9765a94ff22e0ca3afc3e3f4b116de2b67582a',
+    decimals: 16,
+    fullName: 'CGCOINS',
+  },
+  vite: {
+    address: '0x1b793E49237758dBD8b752AFC9Eb4b329d5Da016',
+    decimals: 18,
+    fullName: 'ViteToken',
+  },
+  mot: {
+    address: '0x263c618480dbe35c300d8d5ecda19bbb986acaed',
+    decimals: 18,
+    fullName: 'MOT',
+  },
+  dov: {
+    address: '0xac3211a5025414Af2866FF09c23FC18bc97e79b1',
+    decimals: 18,
+    fullName: 'DOVU',
+  },
+  xbx: {
+    address: '0x2c745664FB4e9528fB32487D834e4aACffF1F027',
+    decimals: 18,
+    fullName: 'Bitex Coin',
+  },
+
 }
