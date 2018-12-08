5705f970aba569d79d3ef79b85854d2de70b8c73
Pasha8914 <uhau-buhau@mail.ru>
2018-12-09 03:25:45 +1200

    изменил расположение кнопок

--- shared/pages/Currency/Currency.js
+++ shared/pages/Currency/Currency.js
@@ -18,6 +18,7 @@ import actions from 'redux/actions'
 
 import { withRouter } from 'react-router'
 import { FormattedMessage } from 'react-intl'
+import CurrencyButton from 'components/controls/CurrencyButton/CurrencyButton'
 
 
 @withRouter
@@ -31,14 +32,13 @@ export default class Currency extends Component {
 
   state = {
     isBalanceFetching: false,
+    isBalanceEmpty: false,
   }
 
   getRows = () => {
     let { match:{ params: { currency, address } }, items } = this.props
     currency = currency.toLowerCase()
 
-    const itemCurrency = items.filter(item => item.currency.toLowerCase() === currency)
-    const selectCurrency = itemCurrency[0]
     return constants.tradeTicker
       .filter(ticker => {
         ticker = ticker.split('-')
@@ -51,8 +51,6 @@ export default class Currency extends Component {
         return {
           from: pair[0],
           to: pair[1],
-          itemCurrency,
-          selectCurrency,
         }
       })
   }
@@ -96,6 +94,39 @@ export default class Currency extends Component {
     this.handleReloadBalance()
   }
 
+  handleReceive = () => {
+    let { match:{ params: { currency } }, items } = this.props
+    const itemCurrency = items.filter(item => item.currency.toLowerCase() === currency)[0]
+    const address = itemCurrency.address
+
+    currency = currency.toUpperCase()
+
+    actions.modals.open(constants.modals.ReceiveModal, {
+      currency,
+      address,
+  })
+}
+
+  handleWithdraw = () => {
+    let { match:{ params: { currency } }, items } = this.props
+    const itemCurrency = items.filter(item => item.currency.toLowerCase() === currency)[0]
+    
+    actions.analytics.dataEvent(`balances-withdraw-${currency.toLowerCase()}`)
+    actions.modals.open(constants.modals.Withdraw, {
+      ...itemCurrency,
+    })
+  }
+
+  checkBalance = () => {
+    const { selectCurrency:{ balance } } = this.props
+    if (balance === 0) {
+      this.setState({
+        isBalanceEmpty: true,
+      })
+    }
+  }
+
+
   render() {
     const { match: { params: { currency } } } = this.props
     const { isBalanceEmpty } = this.state
@@ -104,14 +135,26 @@ export default class Currency extends Component {
       <section styleName={isMobile ? 'currencyMobileSection' : 'currencyMediaSection'}>
         <PageHeadline>
           <Fragment>
-            <div styleName="currencyTitle">
-              <Title>{currency}</Title>
-            </div>
             <SubTitle>{currency.toUpperCase()} Trade</SubTitle>
           </Fragment>
           <div styleName="currencyBalance">
             <FormattedMessage id="Currency101" defaultMessage="Balance: " />
             <span styleName="currencyBalanceValue">{(String(balance).length > 5 ? balance.toFixed(5) : balance) || 0} {currency}</span>
+          </div>
+          <div style={{ marginTop: '20px', height: '20px' }}>
+            <CurrencyButton
+              wallet="true"
+              onClick={this.handleReceive}
+              data={`currency${currency}`}
+              text={<FormattedMessage id="CurrencyWallet110" defaultMessage="Deposit funds to this address of currency wallet" />} >
+              <FormattedMessage id="Row313" defaultMessage="Deposit" />
+            </CurrencyButton>
+            <CurrencyButton
+              wallet="true"
+              onClick={this.handleWithdraw}
+              text={<FormattedMessage id="CurrencyWallet113" defaultMessage="You can not send this asset, because you have a zero balance." />}>
+              <FormattedMessage id="CurrencyWallet100" defaultMessage="Send" />
+            </CurrencyButton>
           </div>
         </PageHeadline>
         <Table
--- shared/pages/Currency/Row/Row.js
+++ shared/pages/Currency/Row/Row.js
@@ -10,7 +10,6 @@ import styles from './Row.scss'
 import Coin from 'components/Coin/Coin'
 import Flip from 'components/controls/Flip/Flip'
 import { FormattedMessage } from 'react-intl'
-import CurrencyButton from 'components/controls/CurrencyButton/CurrencyButton'
 
 
 @cssModules(styles)
@@ -20,62 +19,9 @@ export default class Row extends Component {
     from: PropTypes.string.isRequired,
     to: PropTypes.string.isRequired,
   }
-  state ={
-    isBalanceEmpty: false,
-  }
-
-  componentWillMount() {
-    const { isBalanceEmpty } = this.state
-    this.checkBalance()
-    this.setState({ isBalanceEmpty })
-  }
-
-  handleReceive = () => {
-    const { selectCurrency:{ address, currency } } = this.props
-
-    actions.modals.open(constants.modals.ReceiveModal, {
-      address,
-      currency,
-    })
-  }
-
-  handleWithdraw = () => {
-    const { selectCurrency: {
-      currency,
-      address,
-      contractAddress,
-      decimals,
-      token,
-      balance,
-      unconfirmedBalance,
-    },
-    } = this.props
-
-    actions.analytics.dataEvent(`balances-withdraw-${currency.toLowerCase()}`)
-    actions.modals.open(constants.modals.Withdraw, {
-      currency,
-      address,
-      contractAddress,
-      decimals,
-      token,
-      balance,
-      unconfirmedBalance,
-    })
-  }
-
-  checkBalance = () => {
-    const { selectCurrency:{ balance } } = this.props
-    if (balance === 0) {
-      this.setState({
-        isBalanceEmpty: true,
-      })
-    }
-  }
 
   render() {
-    const { from, to, selectCurrency:{ balance, currency, address } } = this.props
-    const { isBalanceEmpty } = this.state
-    console.log(isBalanceEmpty, balance)
+    const { from, to } = this.props
     return (
       <tr styleName="exchangeTr">
         <td>
@@ -90,20 +36,6 @@ export default class Row extends Component {
           </span>
         </td>
         <td>
-          <CurrencyButton
-            wallet="true"
-            onClick={this.handleReceive}
-            data={`currency${currency}`}
-            text={<FormattedMessage id="CurrencyWallet110" defaultMessage="Deposit funds to this address of currency wallet" />} >
-            <FormattedMessage id="Row313" defaultMessage="Deposit" />
-          </CurrencyButton>
-          <CurrencyButton
-            wallet="true"
-            onClick={this.handleWithdraw}
-            data={isBalanceEmpty && `${currency}`}
-            text={<FormattedMessage id="CurrencyWallet113" defaultMessage="You can not send this asset, because you have a zero balance." />}>
-            <FormattedMessage id="CurrencyWallet100" defaultMessage="Send" />
-          </CurrencyButton>
           <Link styleName="button" to={`${links.home}${from.toLowerCase()}-${to.toLowerCase()}`}>
             <FormattedMessage id="Row35" defaultMessage="Exchange " />
           </Link>
