02c604b158665d6df78fac76f3fee52be0f92018
Aleksey Bykhun <theisease@gmail.com>
2018-12-15 04:16:16 +0300

    partial: use geth.swaponline

--- config/mainnet/web3.js
+++ config/mainnet/web3.js
@@ -1,5 +1,4 @@
 export default {
-  provider: 'http://95.216.187.87:8545',
-  // provider: 'https://geth.swaponline.site',
+  provider: 'https://geth.swaponline.site',
   // provider: 'https://mainnet.infura.io/5lcMmHUURYg8F20GLGSr',
 }
