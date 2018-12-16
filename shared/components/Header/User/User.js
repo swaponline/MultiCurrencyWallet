0c8f33622da1430d4c651c20258b984d8357b852
nikdementev <nikita.pietrov.71@mail.ru>
2018-12-16 20:02:12 +0300

    fix travis

--- shared/components/Header/User/User.js
+++ shared/components/Header/User/User.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import { Redirect } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import actions from 'redux/actions'
 import { connect } from 'redaction'
--- shared/components/Header/User/UserTooltip/UserTooltip.js
+++ shared/components/Header/User/UserTooltip/UserTooltip.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-import { connect } from 'redaction'
-
 import { links } from 'helpers'
 import { Link } from 'react-router-dom'
 
