9bbc75ac3c99f4f75731a05140a6b16bb35ffac2
nthgness <wu-rza@mail.ru>
2018-12-19 01:35:31 +0700

    swap progress: add new notify component

--- shared/pages/Swap/Swap.js
+++ shared/pages/Swap/Swap.js
@@ -2,12 +2,13 @@ import React, { PureComponent } from 'react'
 
 import Swap from 'swap.swap'
 
-import CSSModules from 'react-css-modules'
+import cssModules from 'react-css-modules'
 import styles from './Swap.scss'
 
 import { connect } from 'redaction'
 import { links, constants } from 'helpers'
 import actions from 'redux/actions'
+
 
 import { swapComponents } from './swaps'
 import Share from './Share/Share'
@@ -20,15 +21,14 @@ import DeleteSwapAfterEnd from './DeleteSwapAfterEnd'
 @injectIntl
 @connect(({
   user: { ethData, btcData, /* bchData, */ tokensData, eosData, telosData, nimData, usdtData, ltcData },
-  ipfs: { peer },
 }) => ({
   items: [ ethData, btcData, eosData, telosData, /* bchData, */ ltcData, usdtData /* nimData */ ],
   tokenItems: [ ...Object.keys(tokensData).map(k => (tokensData[k])) ],
   errors: 'api.errors',
   checked: 'api.checked',
-  peer,
 }))
-@CSSModules(styles)
+
+@cssModules(styles, { allowMultiple: true })
 export default class SwapComponent extends PureComponent {
 
   state = {
@@ -90,23 +90,20 @@ export default class SwapComponent extends PureComponent {
   }
 
   render() {
-    const { peer } = this.props
     const { swap, SwapComponent, currencyData } = this.state
+    console.log(this.props)
+    console.log(this.state)
 
-    if (!swap || !SwapComponent || !peer) {
+    if (!swap || !SwapComponent) {
       return null
     }
 
     return (
+
       <div styleName="swap">
-        <SwapComponent swap={swap} currencyData={currencyData}>
+        <SwapComponent swap={swap} currencyData={currencyData} styles={styles}>
           <Share flow={swap.flow} />
           <EmergencySave flow={swap.flow} />
-          {
-            peer === swap.owner.peer && (
-              <DeleteSwapAfterEnd swap={swap} />
-            )
-          }
         </SwapComponent>
       </div>
     )
