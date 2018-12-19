2f863083cd368b40ad07feb85325a79d6248f55e
Pasha8914 <uhau-buhau@mail.ru>
2018-12-19 12:26:23 +1200

    правки по ревью

--- shared/redux/actions/pairs.js
+++ shared/redux/actions/pairs.js
@@ -10,26 +10,26 @@ const selectPair = (value) => {
   const selectedItems = items.filter(item => pairs[value].includes(item.name))
 
   reducers.currencies.addSelectedItems(selectedItems)
-
 }
+
+const erc20Tokens = Object.keys(config.erc20)
+  .map(key => key.toUpperCase())
 
 const pairs = {
   eth: ['ETH', 'LTC', 'BTC'],
-  btc: ['BTC', 'EOS', 'ETH', 'LTC',
-    ...(Object.keys(config.erc20)
-      .map(key => key.toUpperCase())) ],
+  btc: ['BTC', 'EOS', 'ETH', 'LTC', ...erc20Tokens ],
   eos: ['EOS', 'BTC'],
   ltc: ['BTC', 'LTC', 'ETH'],
-  usdt: ['USDT', ...(Object.keys(config.erc20)
-    .map(key => key.toUpperCase())) ],
+  usdt: ['USDT', ...erc20Tokens],
 }
+
 Object.keys(config.erc20)
   .forEach(key => {
-    pairs[key] = ['BTC', 'USDT', ...(Object.keys(config.erc20)
-      .map(key => key.toUpperCase()))]
+    pairs[key] = ['BTC', 'USDT', ...erc20Tokens]
   })
 
 
 export default {
   selectPair,
+  chooseCurrencyList
 }
