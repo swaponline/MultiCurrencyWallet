fead5baa1b5e624bfd30139f29a2f25653db40ae
Igor Berlenko <igor@berlenko.com>
2018-12-23 20:06:42 +0300

    jungle proxy endpoint

--- config/testnet/api.js
+++ config/testnet/api.js
@@ -7,7 +7,7 @@ export default {
   ltc: 'https://ltctest.coinapp.io/api',
   eos: {
     chainId: '038f4b0fc8ff18a4f0842a8f0564611f6e96e8535901dd45e43ac8691a1c4dca',
-    httpEndpoint: 'http://dev.cryptolions.io:38888',
+    httpEndpoint: 'https://seven.swap.online/jungle-endpoint',
     registerEndpoint: 'https://seven.swap.online/eos-testnet/buyaccount',
     buyAccountPriceInBTC: 0.01,
     buyAccountPaymentRecipient: 'mnCLLbtNuXfmHHbDunyjqj61o63XjxNCpG',
