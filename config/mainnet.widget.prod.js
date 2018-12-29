069d64155192f095130799e9b6f6037ed408d7cc
shendel <im_shendel@inbox.ru>
2018-12-29 23:06:25 +0300

    mainnet build prod widget command

---
+++ config/mainnet.widget.prod.js
@@ -0,0 +1,25 @@
+import baseConfig from './default'
+import config from './mainnet'
+
+
+const newERC20 = {}
+newERC20[process.argv[3]] = {
+  address: process.argv[2],
+  decimals: Number.parseInt(process.argv[4], 10),
+  fullName: process.argv[5].split('_').join(' '),
+}
+
+export default {
+  env: 'production',
+  entry: 'mainnet',
+  local: 'online',
+  dir: 'mainnet-widget',
+
+  base: './',
+  publicPath: `.${baseConfig.publicPath}`,
+
+  isWidget: true,
+  ...config,
+  erc20: newERC20,
+  erc20token: process.argv[3],
+}
