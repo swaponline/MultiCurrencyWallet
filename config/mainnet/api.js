5081446a19b1140802a4c5a04c455c18bd46479e
ED <ev@aisapr.ru>
2019-01-23 23:18:19 +1000

    wallet: temporal disable ltc

--- config/mainnet/api.js
+++ config/mainnet/api.js
@@ -5,7 +5,7 @@ export default {
   etherscan: 'https://api.etherscan.io/api',
   usdt: 'https://api.omniexplorer.info/',
   // bch: 'https://bch-insight.bitpay.com/api',
-  ltc: 'https://ltc.coinapp.io/api',
+  ltc: 'https://insight.litecore.io/api',
   eos: {
     chainId: 'aca376f206b8fc25a6ed44dbdc66547c36c6c33e3a119ffbeaef943642f0e906',
     httpEndpoint: 'https://api.eosnewyork.io',
