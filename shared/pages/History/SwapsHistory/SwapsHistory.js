e84105bf815ee3a5060053a4b478c345b4e45ba9
Pasha8914 <uhau-buhau@mail.ru>
2019-01-30 12:14:14 +1000

    удалена колонка со статусом о состоянии рефанда, информация о рефанде перекочевала в статус об ордере

--- shared/pages/History/SwapsHistory/SwapsHistory.js
+++ shared/pages/History/SwapsHistory/SwapsHistory.js
@@ -12,7 +12,6 @@ const titles = [
   <FormattedMessage id="SwapHisrory16" defaultMessage="You buy" />,
   <FormattedMessage id="SwapHisrory17" defaultMessage="You sell" />,
   <FormattedMessage id="SwapHisrory18" defaultMessage="Exchange rate" />,
-  <FormattedMessage id="SwapHisrory19" defaultMessage="Status refund" />,
   <FormattedMessage id="SwapHisrory20" defaultMessage="Status order" />,
   <FormattedMessage id="SwapHisrory211" defaultMessage="Lock time" />,
   <FormattedMessage id="SwapHisrory22" defaultMessage="Link" />,
