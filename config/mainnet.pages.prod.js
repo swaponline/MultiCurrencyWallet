077a503f5aad94da43416d88cad6766a9a2e1b9a
Aleksey Bykhun <theisease@gmail.com>
2019-02-01 15:25:04 +0300

    travis: use gh-pages for deploy builds

--- config/mainnet.pages.prod.js
+++ config/mainnet.pages.prod.js
@@ -6,7 +6,7 @@ export default {
   env: 'production',
   entry: 'mainnet',
   local: 'online',
-  dir: 'mainnet',
+  dir: 'mainnet-pages',
 
   base: 'https://swaponline.github.io/swap.react/',
   publicPath: `https://swaponline.github.io/swap.react${baseConfig.publicPath}`,
