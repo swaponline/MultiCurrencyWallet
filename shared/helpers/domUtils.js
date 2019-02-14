d82b473aa6301e79d4762d2508959010907bb45b
Ilya Starokozhov <grachara@gmail.com>
2019-02-14 20:31:28 +0300

    Inputs: correct styles for moz and Exchange, Withdraw replace , with .

--- shared/helpers/domUtils.js
+++ shared/helpers/domUtils.js
@@ -8,4 +8,13 @@ export const fixBodyOverflow = (fix) => {
   bodySelector.style.overflow = !!fix ? 'hidden' : 'auto'
 }
 
+export const inputReplaceCommaWithDot = (event) => {
+  const isCommaKey = event.key === ','
+
+  if (isCommaKey) {
+    event.preventDefault()
+    document.execCommand('insertHTML', false, '.')
+  }
+}
+
 export const createPortal = (component, customSelector = null) => ReactDOM.createPortal(component, customSelector || portalSelector)
