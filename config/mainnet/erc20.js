7e2bb75a6e14cd1aadfe489124eeb60e1ada5b73
Pasha8914 <uhau-buhau@mail.ru>
2019-02-17 04:10:24 +1000

    new token moscow

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -209,4 +209,9 @@ export default {
     decimals: 18,
     fullName: 'ABYSS',
   },
+  msk: {
+    address: '0xf7ccd7d329aa5b399806bf5351daca18437e0391',
+    decimals: 0,
+    fullName: 'Moscow',
+  },
 }
