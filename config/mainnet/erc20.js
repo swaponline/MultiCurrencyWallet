7dec72d52088d92a95af8cb2c7eecf4af325816d
Pasha8914 <uhau-buhau@mail.ru>
2019-02-17 12:28:15 +1000

    testnet

--- config/mainnet/erc20.js
+++ config/mainnet/erc20.js
@@ -209,9 +209,4 @@ export default {
     decimals: 18,
     fullName: 'ABYSS',
   },
-  msk: {
-    address: '0xf7ccd7d329aa5b399806bf5351daca18437e0391',
-    decimals: 0,
-    fullName: 'Moscow',
-  },
 }
