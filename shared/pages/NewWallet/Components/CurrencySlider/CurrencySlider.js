2b35a1227ea58c09cfc0b9e27b92bddec115e79d
nthgness <wu-rza@mail.ru>
2019-03-08 00:00:25 +0700

    update components, add new blocks

--- shared/pages/NewWallet/Components/CurrencySlider/CurrencySlider.js
+++ shared/pages/NewWallet/Components/CurrencySlider/CurrencySlider.js
@@ -3,6 +3,14 @@ import React, { Component, Fragment } from 'react'
 import styles from './CurrencySlider.scss'
 import cssModules from 'react-css-modules'
 
+import usdt from './images/usdt.svg'
+import trx from './images/trx.svg'
+import btc from './images/btc.svg'
+import ltc from './images/ltc.svg'
+import dash from './images/dash.svg'
+import xrp from './images/xrp.svg'
+import fire from './images/fire.svg'
+
 
 @cssModules(styles)
 export default class CurrencySlider extends Component {
@@ -13,7 +21,33 @@ export default class CurrencySlider extends Component {
           <h3 styleName="availableCurrencies">
             Already available for exchange
           </h3>
-          <div>
+          <div styleName="currencyListWrap">
+            <ul styleName="currencyList">
+              <li styleName="currencyListItem">
+                <img src={usdt} alt="" />
+              </li>
+              <li styleName="currencyListItem">
+                <img src={trx} alt="" />
+              </li>
+              <li styleName="currencyListItem">
+                <img src={btc} alt="" />
+              </li>
+              <li styleName="currencyListItem">
+                <span styleName="fireItem">
+                  <img src={fire} alt="" />
+                </span>
+                <img src={ltc} alt="" />
+              </li>
+              <li styleName="currencyListItem">
+                <img src={dash} alt="" />
+              </li>
+              <li styleName="currencyListItem">
+                <img src={xrp} alt="" />
+              </li>
+            </ul>
+            <a href="#" styleName="currencyAdd">
+              +
+            </a>
           </div>
         </div>
       </Fragment>
--- shared/pages/NewWallet/NewWallet.js
+++ shared/pages/NewWallet/NewWallet.js
@@ -3,23 +3,16 @@ import React, { Component } from 'react'
 import styles from './NewWallet.scss'
 import cssModules from 'react-css-modules'
 
-import NewHeader from './NewHeader/NewHeader'
-import Side from './Side/Side'
-import CurrencySlider from './Components/CurrencySlider/CurrencySlider'
-import PromoText from './Components/PromoText/PromoText'
-import ExchangeBlock from './Components/SwapWindow/ExchangeBlock'
-
+import Side from './Components/Side/Side'
+import Promo from './Components/Promo/Promo'
 
 @cssModules(styles)
 export default class NewWallet extends Component {
   render() {
     return (
-      <div>
-        <NewHeader />
+      <div styleName="container">
+        <Promo />
         <Side />
-        <PromoText />
-        <CurrencySlider />
-        <ExchangeBlock />
       </div>
     )
   }
