3216c5006b8db735e4742ca9d363c091a8601c7b
Pasha8914 <uhau-buhau@mail.ru>
2019-03-15 21:30:24 +1000

    отмена свапа в случае, если у маркет мейкера не хватает на его выполнение

--- shared/components/Header/User/UserTooltip/UserTooltip.js
+++ shared/components/Header/User/UserTooltip/UserTooltip.js
@@ -3,12 +3,13 @@ import PropTypes from 'prop-types'
 
 import { connect } from 'redaction'
 
-import { links } from 'helpers'
+import helpers, { constants, links } from 'helpers'
 import { Link } from 'react-router-dom'
 
 import styles from './UserTooltip.scss'
 import CSSModules from 'react-css-modules'
 import ArrowRightSvg from './images/arrow-right.svg'
+import { BigNumber } from 'bignumber.js'
 
 import { TimerButton } from 'components/controls'
 import { FormattedMessage } from 'react-intl'
@@ -16,6 +17,12 @@ import { FormattedMessage } from 'react-intl'
 import config from 'app-config'
 
 
+@connect(({
+  user: { ethData, btcData, /* bchData, */ tokensData, eosData, telosData, nimData, usdtData, ltcData },
+}) => ({
+  currenciesData: [ ethData, btcData, eosData, telosData, /* bchData, */ ltcData, usdtData /* nimData */ ],
+  tokensData: [ ...Object.keys(tokensData).map(k => (tokensData[k])) ],
+}))
 @CSSModules(styles)
 export default class UserTooltip extends Component {
 
@@ -26,6 +33,40 @@ export default class UserTooltip extends Component {
     acceptRequest: PropTypes.func.isRequired,
   }
 
+  constructor({ tokensData, currenciesData }) {
+
+    super()
+
+    const allCurrencyies = currenciesData.concat(tokensData)
+    const estimatedFeeValues = {}
+
+    this.state = {
+      allCurrencyies,
+      estimatedFeeValues,
+    }
+    this.setEstimatedFeeValues()
+  }
+
+  setEstimatedFeeValues = async () => {
+    const { estimatedFeeValues } = this.state
+    let newEstimatedFeeValues = { ...estimatedFeeValues }
+
+    for await (let item of constants.coinsWithDynamicFee) { // eslint-disable-line
+      try {
+        const newValue = await helpers[item].estimateFeeValue({ method: 'swap', speed: 'fast' })
+        if (newValue) {
+          newEstimatedFeeValues[item] = newValue
+        }
+      } catch (error) {
+        console.error('Set Estimated Fee Values in for error: ', error)
+      }
+    }
+    return this.setState({
+      estimatedFeeValues: newEstimatedFeeValues,
+    })
+  }
+
+
   render() {
     const { feeds, peer: mePeer } = this.props
 
@@ -35,6 +76,13 @@ export default class UserTooltip extends Component {
         { feeds.length < 3  ? (
           feeds.map(row => {
             const { request, content: { buyAmount, buyCurrency, sellAmount, sellCurrency }, id, peer: ownerPeer } = row
+            const currencyBalance = this.state.allCurrencyies.filter(item => item.currency === sellCurrency)
+            const sellAmounPlusFee = BigNumber(this.state.estimatedFeeValues[sellCurrency.toLowerCase()]).plus(sellAmount)
+
+            if (BigNumber(sellAmounPlusFee).isGreaterThan(currencyBalance[0].balance)) {
+              this.props.declineRequest(id, request[0].participant.peer)
+              return console.error('Not enought money for the swap')
+            }
 
             return (
               mePeer === ownerPeer &&
