b477850a54d65ab26bb3acb3f8682836cff9a56e
Pasha8914 <uhau-buhau@mail.ru>
2019-03-15 13:07:18 +1000

    временной фильтр для покинутых свапов

--- shared/helpers/handleGoTrade.js
+++ shared/helpers/handleGoTrade.js
@@ -1,19 +1,32 @@
 import SwapApp, { util } from 'swap.app'
 import actions from 'redux/actions'
+import moment from 'moment/moment'
 
 
 const getDeclinedExistedSwapIndex = ({ currency, decline }) => {
 
-  const length = decline.length - 1
-  const declineSwap = actions.core.getSwapById(decline[length])
+  const date = Date.now() / 1000
 
-  for (let i = 0; i <= length; i++) {
-    if (declineSwap.flow.state.isFinished === true) {
+  const indexOfDecline = decline.length - 1
+  const declineSwap = actions.core.getSwapById(decline[indexOfDecline])
+
+  const itemState = declineSwap.flow.state
+
+  const values = itemState.btcScriptValues
+    || itemState.ltcScriptValues
+    || itemState.usdtScriptValues
+    || itemState.scriptValues
+
+  const lockTime = moment.unix(values.lockTime || date)._i / 1000
+  const timeSinceLock = date - lockTime
+
+  for (let i = 0; i <= indexOfDecline; i++) {
+    if (declineSwap.flow.state.isFinished === true || timeSinceLock > 259200) { // 259200 3 дня в секундах
       actions.core.forgetOrders(decline[i])
     } else if (declineSwap.sellCurrency === currency.toUpperCase()
       && !declineSwap.isSwapExist
       && !declineSwap.isMy) {
-      return length
+      return indexOfDecline
     } else {
       return false
     }
