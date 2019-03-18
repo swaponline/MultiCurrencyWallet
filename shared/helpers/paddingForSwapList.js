33856af625af86c9a8d21333bddb7fc600509185
nthgness <wu-rza@mail.ru>
2019-03-18 20:12:04 +0700

    fix: swapList, progress circle, leave message

--- shared/helpers/paddingForSwapList.js
+++ shared/helpers/paddingForSwapList.js
@@ -3,15 +3,15 @@ const getPaddingValue = ({ step }) => {
     const getPaddingValue = 60 * step
     return getPaddingValue
   }
-  if (step > 2 && step < 5) {
+  if (step > 2 && step <= 5) {
     const getPaddingValue = 120
     return getPaddingValue
   }
-  if (step > 5 && step < 7) {
+  if (step >= 6 && step < 7) {
     const getPaddingValue = 180
     return getPaddingValue
   }
-  if (step > 7) {
+  if (step >= 7) {
     const getPaddingValue = 210
     return getPaddingValue
   }
