c6e45f3a9d93c8e3a8f798b8762a6c6ddd7c9c5f
shendel <im_shendel@inbox.ru>
2019-04-10 18:44:59 +0300

    cache helpers

---
+++ shared/helpers/cache.js
@@ -0,0 +1,39 @@
+import getUnixTimeStamp from './getUnixTimeStamp'
+
+
+const cacheStorage = {}
+
+const cacheStorageGet = (cachePart, cacheKey) => {
+  if (cacheStorage[cachePart]
+    && cacheStorage[cachePart][cacheKey]
+    && (cacheStorage[cachePart][cacheKey].timeout > getUnixTimeStamp())
+  ) {
+    return cacheStorage[cachePart][cacheKey].value
+  }
+  return false
+}
+
+const cacheStorageSet = (cachePart, cacheKey, cacheValue, cacheTimeout) => {
+  if (!cacheStorage[cachePart]) cacheStorage[cachePart] = {}
+  cacheStorage[cachePart][cacheKey] = {
+    timeout: getUnixTimeStamp() + cacheTimeout,
+    value: cacheValue,
+  }
+}
+
+const cacheStorageClear = (cachePart, cacheKey) => {
+  if (cacheStorage[cachePart]) {
+    cacheStorage[cachePart][cacheKey] = false
+  }
+}
+
+const cacheStorageClearPart = (cachePart) => {
+  cacheStorage[cachePart] = false
+}
+
+export {
+  cacheStorageGet,
+  cacheStorageSet,
+  cacheStorageClear,
+  cacheStorageClearPart,
+}
