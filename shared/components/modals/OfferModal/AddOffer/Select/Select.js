296ca37f0742d6769ca7ba95c1711c330f026bae
nthgness <wu-rza@mail.ru>
2019-04-16 05:43:07 +0700

    fix constrols

--- shared/components/modals/OfferModal/AddOffer/Select/Select.js
+++ shared/components/modals/OfferModal/AddOffer/Select/Select.js
@@ -7,22 +7,18 @@ import Switching from 'components/controls/Switching/Switching'
 import BigNumber from 'bignumber.js'
 
 
-const Select = ({ balance, currency, changeBalance, switching, all, estimatedFeeValues, ...props }) => (
-  <Fragment>
-    {
-      props.isExchange
-        ? ''
-        : (
-          <FieldLabel inRow >
-            <FormattedMessage id="Select10" defaultMessage="Available amount to sell" />
-          </FieldLabel>
-        )
-    }
-    <div styleName="groupField">
-      <p>{currency.toUpperCase()} { balance ? parseFloat(balance).toFixed(5) : 0.00 }</p>
-      <div styleName="cell" onClick={() => changeBalance(BigNumber(balance).div(10))}>
-        <FormattedMessage id="Select15" defaultMessage="Sell " />
-        1/10
+const Select = ({ balance, currency, changeBalance, switching, all, estimatedFeeValues, ...props }) => {
+  const balanceToRender = BigNumber(balance).dp(6, BigNumber.ROUND_CEIL)
+  return (
+    <Fragment>
+      <div styleName="groupField">
+        <div styleName="group">
+          <span styleName="cell" onClick={() => changeBalance(BigNumber(balance).div(4))}><FormattedMessage id="Select23" defaultMessage="25%" /></span>
+          <span styleName="cell" onClick={() => changeBalance(BigNumber(balance).div(4))}><FormattedMessage id="Select25" defaultMessage="50%" /></span>
+          <span styleName="cell" onClick={() => changeBalance(BigNumber(balance).div(2))}><FormattedMessage id="Select30" defaultMessage="75%" /></span>
+          <span styleName="cell" onClick={() => changeBalance(BigNumber(balance).div(2))}><FormattedMessage id="Select40" defaultMessage="100%" /></span>
+        </div>
+        <Switching onClick={switching} />
       </div>
       <div styleName="cell" onClick={() => changeBalance(BigNumber(balance).div(4))}><FormattedMessage id="Select26" defaultMessage="1/4" /></div>
       <div styleName="cell" onClick={() => changeBalance(BigNumber(balance).div(2))}><FormattedMessage id="Select27" defaultMessage="1/2" /></div>
@@ -39,8 +35,8 @@ const Select = ({ balance, currency, changeBalance, switching, all, estimatedFee
         </div>
       }
       <Switching onClick={switching} />
-    </div>
-  </Fragment>
-)
+    </Fragment>
+  )
+}
 
 export default cssModules(Select, styles)
