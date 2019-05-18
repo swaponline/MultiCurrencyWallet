ad76dc50ea1a53c87768e5f76ec26f4221973c60
ED <ev@aisapr.ru>
2019-05-19 02:07:45 +1000

    QR: fix network address for bch

--- shared/components/QR/QR.js
+++ shared/components/QR/QR.js
@@ -19,9 +19,12 @@ export default class QR extends PureComponent {
   render() {
     const { network, address, size } = this.props
 
+    const addressHasNetwork = /:/.test(address)
+    const networkValue = addressHasNetwork ? '' : `${network}:`
+
     return (
       <img
-        src={`https://chart.googleapis.com/chart?chs=${size}x${size}&cht=qr&chl=${network}:${address}`}
+        src={`https://chart.googleapis.com/chart?chs=${size}x${size}&cht=qr&chl=${networkValue}${address}`}
         alt={`${network}: ${address}`}
       />
     )
