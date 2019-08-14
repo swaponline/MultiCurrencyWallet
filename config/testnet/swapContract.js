569869ed25f070e0f60d3e8dd14326a5b26c6ee7
shendel <im_shendel@inbox.ru>
2019-08-14 15:18:04 +0300

    update ERC20 contract - support USDT

--- config/testnet/swapContract.js
+++ config/testnet/swapContract.js
@@ -1,5 +1,5 @@
 export default {
-  erc20: '0x750b5275996946FBf74889E446cC8D5f4f552eF4',
+  erc20: '0xc5febf8C2E11f6fC85897b0134A79175b8410b4f',
   eth: '0x5937f7cDa3670A6fA111Ae83Ab4A39Cfb5b89EC1',
   eos: 'swaponline42',
   reputationOracle: '0x6260B5ef52d72732674fF4BDE3B37a4222dB1785',
