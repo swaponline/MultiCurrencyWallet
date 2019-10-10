56948aba86ea6dcdd97a47bf8d2cb76e6f2e54e9
shendel <im_shendel@inbox.ru>
2019-10-10 12:57:57 +0300

    babel 7 + updated depency

---
+++ babel.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "presets": [ "@babel/react",  ["@babel/env", {
+    "targets": {
+     "browsers": [ ">0.25%", "not ie 11", "not op_mini all"]
+    }
+  }], "@babel/preset-typescript" ],
+  "env": {
+   "production": {
+      "presets": ["minify"]
+    }
+  },
+  "plugins": [
+    //"syntax-trailing-function-commas",
+    ["@babel/plugin-proposal-decorators", { "legacy": true }],
+    "@babel/plugin-proposal-class-properties",
+    "@babel/plugin-proposal-function-bind",
+    "@babel/plugin-transform-destructuring",
+    "@babel/plugin-proposal-object-rest-spread",
+    "@babel/plugin-transform-runtime",
+    "@babel/plugin-syntax-dynamic-import",
+    ["@babel/plugin-transform-modules-commonjs", {
+      "allowTopLevelThis": true
+    }],
+    "@babel/plugin-proposal-function-sent",
+    "@babel/plugin-proposal-throw-expressions"
+  ]
+}
