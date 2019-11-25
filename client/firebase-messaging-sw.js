a7563587f58ce975dacb607d60442015da90b596
Ilya Starokozhov <grachara@gmail.com>
2019-11-25 22:01:31 +0300

    Add default networkFirst offline handling

--- client/firebase-messaging-sw.js
+++ client/firebase-messaging-sw.js
@@ -7,4 +7,17 @@ if( 'function' === typeof importScripts) {
   })
 
   const messaging = firebase.messaging()
+
+  importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+
+  if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+
+    workbox.routing.registerRoute(
+      /\.js$/,
+      new workbox.strategies.NetworkFirst()
+    );
+  } else {
+    console.log(`Boo! Workbox didn't load 😬`);
+  }
 }
