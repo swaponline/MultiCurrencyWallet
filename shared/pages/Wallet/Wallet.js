77392591edee435debf3abc08acc46e7d35da842
nthgness <zalunin.alexey@gmail.com>
2019-12-12 20:01:24 +0700

    add optimisation for balanceForm component

--- shared/pages/Wallet/Wallet.js
+++ shared/pages/Wallet/Wallet.js
@@ -396,7 +396,7 @@ export default class Wallet extends Component {
             !isFetching && !isNaN(usdBalance) ? (
               <div className="data-tut-store" styleName="walletContent" >
                 <div styleName={`walletBalance ${activeView === 0 ? 'active' : ''}`}>
-                  <BalanceForm usdBalance={usdBalance} currencyBalance={btcBalance} {...this.state} handleReceive={this.handleModalOpen} handleWithdraw={this.handleModalOpen} currency="btc" />
+                  <BalanceForm usdBalance={usdBalance} currencyBalance={btcBalance} handleReceive={this.handleModalOpen} handleWithdraw={this.handleModalOpen} currency="btc" />
                   {exchangeForm &&
                     <div styleName="exchangeForm">
                       <ParticalClosure {...this.props} isOnlyForm />
