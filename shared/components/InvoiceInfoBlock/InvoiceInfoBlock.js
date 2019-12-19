403e6e9c48ce52c7453914a1adc1099f6906162b
shendel <im_shendel@inbox.ru>
2019-12-19 13:41:48 +0300

    pay link for desktop wallet

--- shared/components/InvoiceInfoBlock/InvoiceInfoBlock.js
+++ shared/components/InvoiceInfoBlock/InvoiceInfoBlock.js
@@ -9,16 +9,22 @@ import { FormattedMessage } from 'react-intl'
 const InvoiceInfoBlock = (props) => {
   const { invoiceData } = props
 
-
+  let bip0020link = `bitcoin:${(invoiceData.destination) ? invoiceData.destination : invoiceData.fromAddress}`
+  bip0020link = `${bip0020link}?amount=${invoiceData.amount}`
+  bip0020link = `${bip0020link}&label=Invoice-${invoiceData.id}-${invoiceData.invoiceNumber}`
+  if (invoiceData.label) bip0020link = `${bip0020link}&message=${encodeURI(invoiceData.label)}`
 
   return (
     <div styleName='invoiceInfoBlock'>
       <h4>
-        <FormattedMessage id='InvoiceInfoBlockTitle' defaultMessage='Оплата инвойса #{id}-{invoiceNumber}' values={invoiceData} />
+        <FormattedMessage id="InvoiceInfoBlockTitle" defaultMessage="Оплата инвойса #{id}-{invoiceNumber}" values={invoiceData} />
       </h4>
       {invoiceData.label && 
         <span>{invoiceData.label}</span>
       }
+      <a href={bip0020link}>
+        <FormattedMessage id="InvoiceInfoBlockOpenDesktopApp" defaultMessage="Оплатить с внешнего кошелька" />
+      </a>
       <hr />
     </div>
   )
