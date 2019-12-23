eaa250ce69ead0747d96a999a5418e3fe046b061
shendel <im_shendel@inbox.ru>
2019-12-23 13:55:32 +0300

    резервные апи для битпей и эзерскан

--- config/testnet/api.js
+++ config/testnet/api.js
@@ -1,9 +1,15 @@
 export default {
   horizon: 'https://horizon-testnet.stellar.org',
   blocktrail: 'https://api.blocktrail.com/v1/tBTC',
-  bitpay: 'https://test-insight.swap.online/insight-api',
+  bitpay: [
+    'https://invoices.swaponline.site/proxybitpay-testnet',
+    'https://test-insight.swap.online/insight-api',
+  ],
   // bitpay: 'https://test-insight.bitpay.com/api',
-  etherscan: 'https://rinkeby.etherscan.io/api',
+  etherscan: [
+    'https://invoices.swaponline.site/proxyetherscan-testnet',
+    'https://rinkeby.etherscan.io/api',
+  ],
   // usdtOmni: 'https://api.omniexplorer.info/',
   bch: 'https://trest.bitcoin.com/v2',
   ltc: 'https://testnet.litecore.io/api',
