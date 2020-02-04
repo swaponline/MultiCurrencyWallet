43a62445fec812576db58f8df24a01e50c9973a5
Rustam Trubnikov <trubnikov.r@list.ru>
2020-02-04 17:20:38 +0500

    add urls and CopyToClipboard

--- shared/components/modals/Share/Share.js
+++ shared/components/modals/Share/Share.js
@@ -3,6 +3,8 @@ import { Modal } from 'components/modal'
 import styles from './Share.scss'
 import { FormattedMessage, defineMessages, injectIntl } from 'react-intl'
 import cssModules from 'react-css-modules'
+import actions from 'redux/actions'
+import CopyToClipboard from 'react-copy-to-clipboard'
 
 
 @injectIntl
@@ -14,33 +16,38 @@ export default class Share extends Component {
     const { props: { data: { link, title } } } = this
 
     if (navigator.share) {
-
       navigator.share({
         title: title,
         url: link
-      }).catch(console.error);
+      }).catch(console.error)
+      actions.modals.closeAll()
+
     }
 
   }
 
   render() {
-    const { props: { data: { link } } } = this
+    const { props: { data: { link, title } } } = this
+
+    if (!link && data) {
+      // @ToDo return error
+    }
 
     let name = 'ShareModal';
-    let title = 'Share';
-    
+    let modalTitle = 'Share';
+
     return (
-      <Modal name={name} title={title} >
+      <Modal name={name} title={modalTitle} >
         <span styleName="targets">
-          <a styleName="button">
+          <a styleName="button" href={'https://www.facebook.com/sharer/sharer.php?u=' + encodeURI(link) + '&t=' + title} target="_blank">
             <i styleName="icon" className="fab fa-facebook" />
             <span>Facebook</span>
           </a>
-          <a styleName="button">
+          <a styleName="button" href={'https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url' + encodeURI(link)}>
             <i styleName="icon" className="fab fa-twitter" />
             <span>Twitter</span>
           </a>
-          <a styleName="button">
+          <a styleName="button" href={'https://www.linkedin.com/shareArticle?mini=true&url=' + encodeURI(link) + '&title=' + encodeURIComponent(title)}>
             <i styleName="icon" className="fab fa-linkedin" />
             <span>LinkedIn</span>
           </a>
@@ -51,14 +58,16 @@ export default class Share extends Component {
         </span>
         <div styleName="link">
           <div styleName="pen-url">{link}</div>
-          <button styleName="copy-link">
-            <FormattedMessage
-              id="ShareModal1"
-              defaultMessage="Copy Link"
-            />
-          </button>
+          <CopyToClipboard text={link}>
+            <button styleName="copy-link">
+              <FormattedMessage
+                id="ShareModal1"
+                defaultMessage="Copy Link"
+              />
+            </button>
+          </CopyToClipboard>
         </div>
-      </Modal>
+      </Modal >
     )
   }
 }
