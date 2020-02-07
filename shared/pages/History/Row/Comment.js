8423e1321806360fb4c8378c6fb7080528e4a65b
Rustam Trubnikov <trubnikov.r@list.ru>
2020-02-07 15:49:48 +0500

    textarea full text

--- shared/pages/History/Row/Comment.js
+++ shared/pages/History/Row/Comment.js
@@ -1,44 +1,57 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
-import cssModules from 'react-css-modules'
+import CSSModules from 'react-css-modules'
 import moment from 'moment-with-locales-es6'
-import actions from 'redux/actions'
-
 import styles from './Row.scss'
 
 
-const submitComment = (e, props) => {
-  const { comment, toggleComment, onSubmit, hiddenList, ind } = props
-  e.preventDefault()
+@CSSModules(styles, { allowMultiple: true })
+export default class CommentRow extends React.Component {
+  
+  static propTypes = {
+    label: PropTypes.string.isRequired
+  };
 
-  const comments = { ...hiddenList, [ind]: comment }
+  constructor(props) {
+    super(props);
+    this.commentTextarea = React.createRef();
+  }
 
-  onSubmit(comments)
-  toggleComment(false)
+  submitComment = (e, props) => {
+    const { comment, toggleComment, onSubmit, hiddenList, ind } = props
+    e.preventDefault()
+  
+    const comments = { ...hiddenList, [ind]: comment }
+  
+    onSubmit(comments)
+    toggleComment(false)
+  }
+
+  componentDidUpdate(prevProps) {
+    
+    if (this.props.isOpen && this.props.isOpen !== prevProps.isOpen) {
+      this.handleKeyUp();
+    }
+  }
+
+  handleKeyUp = () => {
+  
+    this.commentTextarea
+          .current.style.cssText = 'height:' + this.commentTextarea.current.scrollHeight + 'px;'  
+  }
+
+  render() {
+    
+    const { comment, label, toggleComment, changeComment, date, isOpen, commentCancel } = this.props
+    return isOpen ?
+      <form styleName="input" onSubmit={(e) => this.submitComment(e, this.props)}>
+        <textarea ref={this.commentTextarea} styleName="commentTextarea" id="commentTextarea" onKeyUp={this.handleKeyUp} onChange={changeComment} value={comment || `${moment(date).format('LLLL')}  ${label}`} ></textarea>
+        <span styleName="submit" onClick={(e) => this.submitComment(e, this.props)}>&#10004;</span>
+        <span styleName="close" onClick={commentCancel}>&times;</span>
+      </form> :
+      <div styleName="date" onDoubleClick={() => toggleComment(true)}>{comment || `${moment(date).format('LLLL')}  ${label}`}</div>
+
+  }
 }
-
-const CommentRow = (props) => {
-
-  const { comment, label, toggleComment, changeComment, date, isOpen, commentCancel } = props
-  return isOpen ?
-    <form styleName="input" onSubmit={(e) => submitComment(e, props)}>
-      <textarea onChange={changeComment} >{comment || `${moment(date).format('LLLL')}  ${label}`}</textarea>
-      <span styleName="submit" onClick={(e) => submitComment(e, props)}>&#10004;</span>
-      <span styleName="close" onClick={commentCancel}>&times;</span>
-    </form> :
-    <div styleName="date" onDoubleClick={() => toggleComment(true)}>{comment || `${moment(date).format('LLLL')}  ${label}`}</div>
-}
-
-
-CommentRow.defaultProps = {
-  label: '',
-}
-
-CommentRow.propTypes = {
-  label: PropTypes.string.isRequired,
-}
-
-export default cssModules(CommentRow, styles, { allowMultiple: true })
 
 
