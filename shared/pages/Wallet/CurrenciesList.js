44e02d65ba7a87ee03c64e850434063c9436716c
shendel <im_shendel@inbox.ru>
2020-03-10 22:47:19 +0300

    re-fix conflits

--- shared/pages/Wallet/CurrenciesList.js
+++ shared/pages/Wallet/CurrenciesList.js
@@ -61,10 +61,11 @@ const CurrenciesList = ({
         rows={tableRows}
         rowRender={(row, index, selectId, handleSelectId) => (
           <Row
-            key={row.currency}
+            key={index}
             index={index}
             getCurrencyUsd={usd => this.getCurrencyUsd(usd)}
             currency={row}
+            itemData={row}
             currencies={currencies}
             infoAboutCurrency={infoAboutCurrency}
             getExCurrencyRate={(currencySymbol, rate) => getExCurrencyRate(currencySymbol, rate)}
