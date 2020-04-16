258cd42cc7392f828e25342f22e3482e02f8a3a6
Ilya Starokozhov <grachara@gmail.com>
2020-04-16 19:53:17 +0300

    Add loader to QR codes

--- shared/components/QR/QR.js
+++ shared/components/QR/QR.js
@@ -1,10 +1,23 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
+import CSSModules from 'react-css-modules'
 import PropTypes from 'prop-types'
+import InlineLoader from 'components/loaders/InlineLoader/InlineLoader'
+
+import styles from './QR.scss'
 
 /**
  * @todo Amount support?
  */
-export default class QR extends PureComponent {
+@CSSModules(styles, { allowMultiple: true })
+export default class QR extends Component {
+
+  constructor(props) {
+    super()
+
+    this.state = {
+      renderQr: false,
+    }
+  }
 
   static propTypes = {
     network: PropTypes.string.isRequired,
@@ -16,24 +29,38 @@ export default class QR extends PureComponent {
     size: 250,
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({ renderQr: true })
+    }, 3000)
+  }
+
   render() {
     let { network, address, size } = this.props
 
+    // eslint-disable-next-line default-case
     switch (network.toLowerCase()) {
       case 'btc (multisig)':
       case 'btc (sms-protected)':
         network = 'btc'
-        break;
+        break
     }
 
     const addressHasNetwork = /:/.test(address)
     const networkValue = addressHasNetwork ? '' : `${network}:`
 
     return (
-      <img
-        src={`https://chart.googleapis.com/chart?chs=${size}x${size}&cht=qr&chl=${networkValue}${address}`}
-        alt={`${network}: ${address}`}
-      />
+      <React.Fragment>
+        <div className={this.state.renderQr ? styles.neverAppear : styles.loader}>
+          <InlineLoader ref={ref => this.loader = ref} />
+        </div>
+        <img
+          src={`https://chart.googleapis.com/chart?chs=${size}x${size}&cht=qr&chl=${networkValue}${address}`}
+          alt={`${network}: ${address}`}
+          className={this.state.renderQr ? '' : styles.hidden}
+          ref={ref => this.QR = ref}
+        />
+      </React.Fragment>
     )
   }
 }
