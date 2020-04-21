acd11613aca4d69656c1f17742bb8af369315e09
shendel <im_shendel@inbox.ru>
2020-04-21 12:50:04 +0300

    rewrite tx info router

--- shared/pages/Transaction/Transaction.js
+++ shared/pages/Transaction/Transaction.js
@@ -3,63 +3,33 @@ import React, { Component, Fragment } from 'react'
 import actions from 'redux/actions'
 import { constants } from 'helpers'
 import getCurrencyKey from "helpers/getCurrencyKey";
-import { FormattedMessage } from 'react-intl'
+import { FormattedMessage, defineMessages, injectIntl } from 'react-intl'
 import getWalletLink from 'helpers/getWalletLink'
-import redirectTo from 'helpers/redirectTo'
+import { links } from 'helpers'
+import TxInfo from './TxInfo'
+import { Modal } from 'components/modal'
+import cssModules from 'react-css-modules'
+import styles from './styles.scss'
 
 
+const labels = defineMessages({
+  Title: {
+    id: 'InfoPay_1',
+    defaultMessage: 'Transaction is completed',
+  },
+})
+
+@injectIntl
+@cssModules({
+  ...styles,
+}, { allowMultiple: true })
 class Transaction extends Component {
+  unmounted = false
 
   constructor(props) {
     super(props)
 
-    this.state = {
-      isFetching: true,
-      infoTx: false,
-    }
-  }
-
-  async fetchTxInfo(currencyKey, txId) {
-
-    const infoTx = await actions[currencyKey].fetchTxInfo(txId, 5*60*1000)
-
-    if(!infoTx) {
-      // Fail parse
-      return
-    }
-    this.setState({
-      isFetching: false,
-      infoTx,
-    }, () => {
-      this.updateTxInfoModal()
-    })
-  }
-
-  updateTxInfoModal() {
     const {
-      history,
-    } = this.props
-
-    const {
-      currency,
-      txId,
-      infoTx,
-      infoModal
-    } = this.state
-
-    infoModal.setState({
-      amount:infoTx.amount,
-      balance:0,
-      oldBalance: infoTx.afterBalance,
-      confirmed: infoTx.confirmed,
-      sender: infoTx.senderAddress,
-      toAddress: infoTx.receiverAddress,
-      isFetching: false,
-    })
-  }
-
-  componentWillMount() {
-    let {
       history,
       match: {
         params: {
@@ -67,68 +37,141 @@ class Transaction extends Component {
           tx: txId = null,
         }
       } = null
-    } = this.props
+    } = props
 
-    if(!txId) {
-      // 404
-      return
-    }
+
 
     const currency = getCurrencyKey(ticker)
 
-    this.setState({
+    this.state = {
       currency,
       ticker,
       txId,
-    }, () => {
-      actions.modals.open(constants.modals.InfoPay, {
-        currency: ticker,
-        txId,
-        onClose: () => {
-          let {
-            infoTx: {
-              senderAddress: walletOne,
-              receiverAddress: walletTwo,
-            },
-          } = this.state
+      isFetching: true,
+      infoTx: false,
+      amount: 0,
+      balance: 0,
+      oldBalance: 0,
+      confirmed: false,
+      sender: ``,
+      toAddress: ``,
+    }
+  }
 
-          const wallets = []
-          if (walletOne instanceof Array) {
-            walletOne.forEach((wallet) => wallets.push(wallet))
-          } else wallets.push(walletOne)
-          
-          if (walletTwo instanceof Array) {
-            walletTwo.forEach((wallet) => wallets.push(wallet))
-          } else wallets.push(walletTwo)
+  async fetchTxInfo(currencyKey, txId) {
+    const infoTx = await actions[currencyKey].fetchTxInfo(txId, 5*60*1000)
 
-          const walletLink = getWalletLink(ticker, wallets)
-          redirectTo((walletLink) ? walletLink : '/')
-        },
-        isFetching: true,
-        onFetching: (infoModal) => {
-          this.setState({
-            infoModal,
-          }, () => {
-            this.fetchTxInfo(currency, txId)
-          })
-        }
-      }) 
-      
-      
-    })
+    if(!infoTx) {
+      // Fail parse
+      return
+    }
+
+    if (!this.unmounted) {
+      const {
+        amount,
+        afterBalance: oldBalance,
+        confirmed,
+        senderAddress: sender,
+        receiverAddress: toAddress,
+      } = infoTx
+
+      this.setState({
+        isFetching: false,
+        infoTx,
+        amount,
+        balance:0,
+        oldBalance,
+        confirmed,
+        sender,
+        toAddress,
+      })
+    }
+  }
+
+  componentDidMount() {
+    const {
+      currency,
+      txId,
+    } = this.state
+
+    if(!txId) {
+      history.push(links.notFound)
+      return
+    }
+
+    this.fetchTxInfo(currency, txId)
+
+    if (typeof document !== 'undefined') {
+      document.body.classList.add('overflowY-hidden-force')
+    }
+  }
+
+  handleClose = () => {
+    const { history } = this.props
+
+    let {
+      infoTx: {
+        senderAddress: walletOne,
+        receiverAddress: walletTwo,
+      },
+      ticker,
+    } = this.state
+
+    const wallets = []
+    if (walletOne instanceof Array) {
+      walletOne.forEach((wallet) => wallets.push(wallet))
+    } else wallets.push(walletOne)
+    
+    if (walletTwo instanceof Array) {
+      walletTwo.forEach((wallet) => wallets.push(wallet))
+    } else wallets.push(walletTwo)
+
+    const walletLink = getWalletLink(ticker, wallets)
+    history.push((walletLink) ? walletLink : '/')
+  }
+
+  componentWillUnmount() {
+    this.unmounted = true
+
+    if (typeof document !== 'undefined') {
+      document.body.classList.remove('overflowY-hidden-force')
+    }
   }
 
   render() {
-    const { isFetching } = this.state
-    
-    if (isFetching) {
-      return (
-        <div>
-          <FormattedMessage id="Transaction_Pags_Fetching" defaultMessage="Fetching..." />
-        </div>
-      )
-    }
-    return (null);
+    const {
+      intl,
+    } = this.props
+
+    const {
+      isFetching,
+      txId,
+      currency,
+      amount,
+      balance,
+      oldBalance,
+      confirmed,
+      sender,
+      toAddress,
+    } = this.state
+
+    return (
+      <div styleName="holder">
+      {/*<Modal name="TxInfoBlock" title={intl.formatMessage(labels.Title)} onClose={this.handleClose} showCloseButton={true}> */}
+          <TxInfo 
+            isFetching={isFetching}
+            txId={txId}
+            currency={currency}
+            amount={amount}
+            balance={balance}
+            oldBalance={oldBalance}
+            confirmed={confirmed}
+            sender={sender}
+            toAddress={toAddress}
+          />
+      {/* </Modal> */}
+      </div>
+    )
   }
 }
 
