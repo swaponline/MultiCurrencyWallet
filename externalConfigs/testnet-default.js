d50ae5b5b9d26a355e8b06ce1ebefe61dbbc920a
shendel <im_shendel@inbox.ru>
2020-05-07 15:22:16 +0300

    config example (with min fee amount)

--- externalConfigs/testnet-default.js
+++ externalConfigs/testnet-default.js
@@ -4,14 +4,17 @@ window.widgetERC20Comisions = {
   btc: {
     fee: 5,
     address: '2MuXz9BErMbWmoTshGgkjd7aMHeaxV8Bdkk',
+    min: 0.00001
   },
   eth: {
     fee: 7,
     address: '0x276747801B0dbb7ba04685BA27102F1B27Ca0815',
+    min: 0.01
   },
   erc20: {
     fee: 6,
     address: '0x276747801B0dbb7ba04685BA27102F1B27Ca0815',
+    min: 1,
   }
 }
 
