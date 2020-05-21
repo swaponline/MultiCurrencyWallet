0e9bcd6694f801f1911940a7ef388cee0b3b0a78
uhaubuhau894@gmail.com <uhaubuhau894@gmail.com>
2020-05-21 22:45:23 +1000

    Несколько ишью

--- shared/pages/Multisign/Btc/Btc.js
+++ shared/pages/Multisign/Btc/Btc.js
@@ -8,7 +8,7 @@ import { localisedUrl } from 'helpers/locale'
 import { FormattedMessage, injectIntl } from 'react-intl'
 import { withRouter } from 'react-router'
 import actions from 'redux/actions'
-import { links }    from 'helpers'
+import { links } from 'helpers'
 import { getFullOrigin } from 'helpers/links'
 import Button from 'components/controls/Button/Button'
 
@@ -39,12 +39,12 @@ export default class Btc extends PureComponent {
     intl: PropTypes.object.isRequired,
   };
 
-  
+
 
   constructor() {
     console.log('Btc mulsign connected')
     super()
-    
+
     this.timerWaitOnlineJoin = false
     this.state = {
       action: 'none',
@@ -67,7 +67,7 @@ export default class Btc extends PureComponent {
       intl: {
         locale,
       },
-      history, 
+      history,
       location: {
         pathname,
       },
@@ -84,7 +84,7 @@ export default class Btc extends PureComponent {
     action = action.toLowerCase()
 
     if (action === 'join' || action === 'connect') {
-      if (data && data.length==66) {
+      if (data && data.length == 66) {
         const publicKey = data
         const walletData = actions.btcmultisig.login_USER(privateKey, publicKey, true)
 
@@ -93,7 +93,7 @@ export default class Btc extends PureComponent {
           return
         }
 
-        const balance = await actions.btcmultisig.fetchBalance( walletData.address )
+        const balance = await actions.btcmultisig.fetchBalance(walletData.address)
         const myPublicKey = this.props.data.publicKey.toString('hex')
 
         this.setState({
@@ -106,7 +106,7 @@ export default class Btc extends PureComponent {
           myPublicKey,
           joinLink: `${getFullOrigin()}${links.multisign}/btc/connect/${myPublicKey}`,
         })
-        actions.ipfs.onReady( () => {
+        actions.ipfs.onReady(() => {
           this.setState({
             addWalletEnabled: true,
           })
@@ -194,14 +194,14 @@ export default class Btc extends PureComponent {
     console.log('btc multisig join ready', _data)
     const { fromPeer, data } = _data
     const { peer } = this.state
-    if ( fromPeer === peer ) {
+    if (fromPeer === peer) {
       SwapApp.shared().services.room.unsubscribe('btc multisig join ready', this.handleOnlineWalletConnect)
       clearTimeout(this.timerWaitOnlineJoin)
       this.connectWallet('ready')
     }
   }
 
-  handleAddWallet = async() => {
+  handleAddWallet = async () => {
     const { action, myPublicKey, publicKey, peer } = this.state
 
     //If peer is online - try connect via ipfs
@@ -217,14 +217,14 @@ export default class Btc extends PureComponent {
           action: 'onlinejoin',
         })
         SwapApp.shared().services.room.subscribe('btc multisig join ready', this.handleOnlineWalletConnect)
-        SwapApp.shared().services.room.sendMessagePeer( peer, {
+        SwapApp.shared().services.room.sendMessagePeer(peer, {
           event: 'btc multisig join',
           data: {
             publicKey: myPublicKey,
             checkKey: publicKey,
           }
         })
-        this.timerWaitOnlineJoin = setTimeout( () => {
+        this.timerWaitOnlineJoin = setTimeout(() => {
           SwapApp.shared().services.room.unsubscribe('btc multisig join ready', this.handleOnlineWalletConnect)
           this.connectWallet(action)
         }, 10000)
@@ -236,7 +236,7 @@ export default class Btc extends PureComponent {
     )
   }
 
-  handleGoToWallet = async() => {
+  handleGoToWallet = async () => {
     const {
       history,
       intl: {
@@ -248,154 +248,157 @@ export default class Btc extends PureComponent {
     history.push(localisedUrl(locale, `/btc/${address}`))
   }
 
-  handleConfirm = async() => {
+  handleConfirm = async () => {
     const { txRaw, invoice } = this.state
-    this.setState( { isConfirming : true } )
-    const signedTX = await actions.btcmultisig.signMultiSign( txRaw )
-    const txID = await actions.btcmultisig.broadcastTx( signedTX )
+    this.setState({ isConfirming: true })
+    const signedTX = await actions.btcmultisig.signMultiSign(txRaw)
+    const txID = await actions.btcmultisig.broadcastTx(signedTX)
     if (invoice) {
       // @ToDo - payer address
       await actions.invoices.markInvoice(invoice, 'ready', txID)
     }
-    this.setState( {
+    this.setState({
       txID,
       action: 'confirmready',
-    } )
+    })
   }
 
   render() {
     const { action } = this.state
 
     const { wallet, walletBalance, joinLink, addWalletEnabled, waitCreateWallet } = this.state
-    
+
     const { debugShowTXB, debugShowInput, debugShowOutput } = this.state
 
     return (
       <section>
-        { (action === 'onlinejoin') &&
-        <Fragment>
-          <h1>
-            <FormattedMessage id="BTCMS_CreateWalletTitle" defaultMessage="Создание BTC-multisignature кошелька" />
-          </h1>
-          <h3>
-            <FormattedMessage id="BTCMS_WaitOtherSide" defaultMessage="Ожидание второй стороны..." />
-          </h3>
-        </Fragment>
+        {(action === 'onlinejoin') &&
+          <Fragment>
+            <h1>
+              <FormattedMessage id="BTCMS_CreateWalletTitle" defaultMessage="Создание BTC-multisignature кошелька" />
+            </h1>
+            <h3>
+              <FormattedMessage id="BTCMS_WaitOtherSide" defaultMessage="Ожидание второй стороны..." />
+            </h3>
+          </Fragment>
         }
-        { (action === 'join' || action === 'connect') && 
-        <Fragment>
-          <h1>
-            <FormattedMessage id="BTCMS_CreateWalletTitle" defaultMessage="Создание BTC-multisignature кошелька" />
-          </h1>
-          
-          <div>
-            <label>
-              <FormattedMessage id="BTCMS_WalletAddress" defaultMessage="Адрес кошелька:" />
-            </label>
-            <strong>{wallet.address}</strong>
-          </div>
-          <div>
-            <label>
-              <FormattedMessage id="BTCMS_WalletBalance" defaultMessage="Баланс" />
-            </label>
-            <strong>{' '}{walletBalance} BTC</strong>
-          </div>
-          { addWalletEnabled ?
+        {(action === 'join' || action === 'connect') &&
+          <Fragment>
+            <h1>
+              <FormattedMessage id="BTCMS_CreateWalletTitle" defaultMessage="Создание BTC-multisignature кошелька" />
+            </h1>
+
+            <div>
+              <label>
+                <FormattedMessage id="BTCMS_WalletAddress" defaultMessage="Адрес кошелька:" />
+              </label>
+              <strong>{wallet.address}</strong>
+            </div>
+            <div>
+              <label>
+                <FormattedMessage id="BTCMS_WalletBalance" defaultMessage="Баланс" />
+              </label>
+              <strong>{' '}{walletBalance} BTC</strong>
+            </div>
+            {addWalletEnabled ?
               waitCreateWallet ?
-              <Button brand>
-                <FormattedMessage id="BTCMS_CreateWalletWait" defaultMessage="Создание кошелька... Подождите немного" />
-              </Button>
+                <Button brand>
+                  <FormattedMessage id="BTCMS_CreateWalletWait" defaultMessage="Создание кошелька... Подождите немного" />
+                </Button>
+                :
+                <Button brand onClick={this.handleAddWallet}>
+                  <FormattedMessage id="BTCMS_CreateWalletAdd" defaultMessage="Добавить этот кошелек" />
+                </Button>
               :
-              <Button brand onClick={this.handleAddWallet}>
-                <FormattedMessage id="BTCMS_CreateWalletAdd" defaultMessage="Добавить этот кошелек" />
+              <Button brand>
+                <FormattedMessage id="BTCMS_CreateWalletLoading" defaultMessage="Загрузка... Подождите немного" />
               </Button>
-            :
-            <Button brand>
-              <FormattedMessage id="BTCMS_CreateWalletLoading" defaultMessage="Загрузка... Подождите немного" />
-            </Button>
-          }
-        </Fragment>
+            }
+          </Fragment>
         }
-        { (/*action=='confirm' ||*/ action==='confirminvoice') && 
-        <Fragment>
-          <h1>
+        <div styleName="descritonText">
+          <FormattedMessage id="BTCMS_CreateWalletLoading_descroptio" defaultMessage="Funds sent to this wallet cannot be spent without your confirmation (please save your private 12 words passphrase)" />
+        </div>
+        {(/*action=='confirm' ||*/ action === 'confirminvoice') &&
+          <Fragment>
+            <h1>
+              <FormattedMessage id="BTCMS_ConfirmTxTitle" defaultMessage="Подтверждение транзакции" />
+            </h1>
+            <h3>
+              <button onClick={() => { this.setState({ debugShowInput: !debugShowInput }) }}>
+                <FormattedMessage id="BTCMS_ConfirmTxInputs" defaultMessage="Входы транзакции" />
+              </button>
+            </h3>
+            {debugShowInput &&
+              <pre>
+                <code>
+                  {
+                    JSON.stringify(this.state.txData.input, false, 4)
+                  }
+                </code>
+              </pre>
+            }
+            <h3>
+              <button onClick={() => { this.setState({ debugShowOutput: !debugShowOutput }) }}>
+                <FormattedMessage id="BTCMS_ConfirmTxOutputs" defaultMessage="Выходы транзакции" />
+              </button>
+            </h3>
+            {debugShowOutput &&
+              <pre>
+                <code>
+                  {
+                    JSON.stringify(this.state.txData.output, false, 4)
+                  }
+                </code>
+                <code>
+                  {
+                    JSON.stringify(this.state.txData, false, 4)
+                  }
+                </code>
+              </pre>
+            }
+            <div>
+              <Button brand onClick={this.handleConfirm}>
+                <FormattedMessage id="BTCMS_ConfirmTxSign" defaultMessage="Подписать транзакцию" />
+              </Button>
+            </div>
+          </Fragment>
+        }
+        {(action === 'confirmready') &&
+          <Fragment>
             <FormattedMessage id="BTCMS_ConfirmTxTitle" defaultMessage="Подтверждение транзакции" />
-          </h1>
-          <h3>
-            <button onClick={ () => { this.setState({debugShowInput: !debugShowInput}) } }>
-              <FormattedMessage id="BTCMS_ConfirmTxInputs" defaultMessage="Входы транзакции" />
-            </button>
-          </h3>
-          {debugShowInput &&
-          <pre>
-            <code>
-              {
-                JSON.stringify(this.state.txData.input, false, 4)
-              }
-            </code>
-          </pre>
-          }
-          <h3>
-            <button onClick={ () => { this.setState({debugShowOutput: !debugShowOutput}) } }>
-              <FormattedMessage id="BTCMS_ConfirmTxOutputs" defaultMessage="Выходы транзакции" />
-            </button>
-          </h3>
-          {debugShowOutput &&
-          <pre>
-            <code>
-              {
-                JSON.stringify(this.state.txData.output, false, 4)
-              }
-            </code>
-            <code>
-              {
-                JSON.stringify(this.state.txData, false, 4)
-              }
-            </code>
-          </pre>
-          }
-          <div>
-            <Button brand onClick={this.handleConfirm}>
-              <FormattedMessage id="BTCMS_ConfirmTxSign" defaultMessage="Подписать транзакцию" />
-            </Button>
-          </div>
-        </Fragment>
+            <h2>
+              <FormattedMessage id="BTCMS_ConfirmTxReady" defaultMessage="Транзакция подписана и отправлена в блокчейн" />
+            </h2>
+            <div>
+              <Button brand onClick={this.handleGoToWallet}>
+                <FormattedMessage id="BTCMS_ConfirmTxGoToWallet" defaultMessage="Перейти в кошелек" />
+              </Button>
+            </div>
+            <pre>
+              <code>
+                {
+                  JSON.stringify(this.state.txID, false, 4)
+                }
+              </code>
+            </pre>
+
+          </Fragment>
         }
-        { (action === 'confirmready') && 
-        <Fragment>
-          <FormattedMessage id="BTCMS_ConfirmTxTitle" defaultMessage="Подтверждение транзакции" />
-          <h2>
-            <FormattedMessage id="BTCMS_ConfirmTxReady" defaultMessage="Транзакция подписана и отправлена в блокчейн" />
-          </h2>
-          <div>
-            <Button brand onClick={this.handleGoToWallet}>
-              <FormattedMessage id="BTCMS_ConfirmTxGoToWallet" defaultMessage="Перейти в кошелек" />
-            </Button>
-          </div>
-          <pre>
-            <code>
-              {
-                JSON.stringify(this.state.txID, false, 4)
-              }
-            </code>
-          </pre>
-          
-        </Fragment>
-        }
-        { (action === 'linkready' || action === 'ready') && 
-        <Fragment>
-          <h1>
-            <FormattedMessage id="BTCMS_CreateWalletTitle" defaultMessage="Создание BTC-multisignature кошелька" />
-          </h1>
-          { action === 'linkready' && <h2><FormattedMessage id="BTCMS_CreateWalletLinkReady" defaultMessage="Кошелек создан. Отправьте эту ссылку второму владельцу для подтверждения" /></h2> }
-          { action === 'ready' && <h2><FormattedMessage id="BTCMS_CreateWalletReady" defaultMessage="Кошелек создан" /></h2> }
-          { action === 'linkready' && <span>{joinLink}</span> }
-          <div>
-            <Button brand onClick={this.handleGoToWallet}>
-              <FormattedMessage id="BTCMS_CreateWalletReadyButton" defaultMessage="Готово. Открыть кошелек" />
-            </Button>
-          </div>
-        </Fragment>
+        {(action === 'linkready' || action === 'ready') &&
+          <Fragment>
+            <h1>
+              <FormattedMessage id="BTCMS_CreateWalletTitle" defaultMessage="Создание BTC-multisignature кошелька" />
+            </h1>
+            {action === 'linkready' && <h2><FormattedMessage id="BTCMS_CreateWalletLinkReady" defaultMessage="Кошелек создан. Отправьте эту ссылку второму владельцу для подтверждения" /></h2>}
+            {action === 'ready' && <h2><FormattedMessage id="BTCMS_CreateWalletReady" defaultMessage="Кошелек создан" /></h2>}
+            {action === 'linkready' && <span>{joinLink}</span>}
+            <div>
+              <Button brand onClick={this.handleGoToWallet}>
+                <FormattedMessage id="BTCMS_CreateWalletReadyButton" defaultMessage="Готово. Открыть кошелек" />
+              </Button>
+            </div>
+          </Fragment>
         }
       </section>
     )
