e564ea398d12d986ef1720f6419a68386cf78288
5053s <51257210+5053s@users.noreply.github.com>
2020-05-28 02:23:39 -0600

    sumcoin explorer

--- config/mainnet/link.js
+++ config/mainnet/link.js
@@ -2,4 +2,5 @@ export default {
   bitpay: 'https://insight.bitpay.com',
   blockcypher: 'https://live.blockcypher.com/',
   etherscan: 'https://etherscan.io',
+  sumexplorer: 'https://sumexplorer.com',
 }
