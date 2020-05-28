16c9e0d35f273395948a039a9c83d8838edc8bff
5053s <51257210+5053s@users.noreply.github.com>
2020-05-28 01:44:43 -0600

    adds SUM to index.js
    per noxon https://github.com/swaponline/swap.core/pull/498#issuecomment-629234115

--- shared/redux/actions/index.js
+++ shared/redux/actions/index.js
@@ -14,6 +14,7 @@ import filter from './filter'
 
 import btc from './btc'
 import btcmultisig from './btcmultisig'
+import sum from './sum'
 import eth from './eth'
 import token from './token'
 
@@ -53,6 +54,7 @@ export default {
 
   btc,
   btcmultisig,
+  sum,
   eth,
 
   token,
