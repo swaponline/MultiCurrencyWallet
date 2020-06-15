7e9cfd69a31914dad24188292978bf4c3b44c7ab
shendel <im_shendel@inbox.ru>
2020-06-16 01:14:12 +0300

    ls data cache use (draft)

--- shared/helpers/lsDataCache.js
+++ shared/helpers/lsDataCache.js
@@ -1,32 +1,66 @@
 import getUnixTimeStamp from './getUnixTimeStamp'
-import localStorage from './localStorage'
 
 
+const localStorage = window.localStorage
 
 const push = ({ key, data, time }) => {
-  const data = {
+  console.log('lsDataCache push', key, time, data)
+  const cacheData = {
     data,
     time,
     createtime: getUnixTimeStamp(),
   }
 
-  const cacheKeys = localStorage.getItem(`${process.env.ENTRY}:cacheKeys`)
+  let cacheKeys = localStorage.getItem(`${process.env.ENTRY}:cacheKeys`)
   try {
     cacheKeys = JSON.parse(cacheKeys)
   } catch (e) {}
   if (!cacheKeys) cacheKeys = {}
 
-  cacheKeys[key] = data.createtime + data.time
- // localStorage.setItem(`${process.env.ENTRY}:cache:${key}`, JSON.seri
+  cacheKeys[key] = cacheData.createtime + cacheData.time
+  localStorage.setItem(`${process.env.ENTRY}:cache:${key}`, JSON.stringify(cacheData))
+  localStorage.setItem(`${process.env.ENTRY}:cacheKeys`, JSON.stringify(cacheKeys))
 }
 
 const get = (key) => {
+  console.log('lsDataCache get', key)
+  let data = localStorage.getItem(`${process.env.ENTRY}:cache:${key}`)
+  try {
+    data = JSON.parse(data)
+    if (data && data.data) {
+      console.log('exists', data.data)
+      return data.data
+    }
+    return false
+  } catch(e) {
+    return false
+  }
 }
 
-const delete = (key) => {
+const remove = (key) => {
+  console.log('lsDataCache remove', key)
+  localStorage.removeItem(`${process.env.ENTRY}:cache:${key}`)
 }
 
 const cleanup = () => {
+  let cacheKeys = localStorage.getItem(`${process.env.ENTRY}:cacheKeys`)
+  const newKeys = {}
+  try {
+    cacheKeys = JSON.parse(cacheKeys)
+  } catch (e) {}
+  if (!cacheKeys) cacheKeys = {}
+
+  const curTime = getUnixTimeStamp()
+
+  Object.keys(cacheKeys).forEach( (key) => {
+    if (!cacheKeys[key] || (curTime > cacheKeys[key])) {
+      remove(key)
+    } else {
+      newKeys[key] = cacheKeys[key]
+    }
+  })
+
+  localStorage.setItem(`${process.env.ENTRY}:cacheKeys`, JSON.stringify(newKeys))
 }
 
 cleanup()
@@ -34,6 +68,6 @@ cleanup()
 export default {
   push,
   get,
-  delete,
+  remove,
   cleanup,
 }
