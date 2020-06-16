fff5d53d83fc84829da85e24e73b25389accbda0
shendel <im_shendel@inbox.ru>
2020-06-16 10:23:38 +0300

    pin and ms cache tx info

--- shared/helpers/lsDataCache.js
+++ shared/helpers/lsDataCache.js
@@ -4,7 +4,6 @@ import getUnixTimeStamp from './getUnixTimeStamp'
 const localStorage = window.localStorage
 
 const push = ({ key, data, time }) => {
-  console.log('lsDataCache push', key, time, data)
   const cacheData = {
     data,
     time,
@@ -23,12 +22,10 @@ const push = ({ key, data, time }) => {
 }
 
 const get = (key) => {
-  console.log('lsDataCache get', key)
   let data = localStorage.getItem(`${process.env.ENTRY}:cache:${key}`)
   try {
     data = JSON.parse(data)
     if (data && data.data) {
-      console.log('exists', data.data)
       return data.data
     }
     return false
@@ -38,7 +35,6 @@ const get = (key) => {
 }
 
 const remove = (key) => {
-  console.log('lsDataCache remove', key)
   localStorage.removeItem(`${process.env.ENTRY}:cache:${key}`)
 }
 
