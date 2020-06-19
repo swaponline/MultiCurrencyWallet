1f6110b30acb5fd0ffc9c25bd5fe410b8a8c6fed
shendel <im_shendel@inbox.ru>
2020-06-19 11:07:16 +0300

    инпут ввода телефона - подставлять код телефона страны, при выборе из списка

--- shared/components/forms/PhoneInput/index.jsx
+++ shared/components/forms/PhoneInput/index.jsx
@@ -1,19 +1,32 @@
-import React from "react";
-import CSSModules from "react-css-modules";
+import React from 'react'
+import CSSModules from 'react-css-modules'
 
-import PnoneLibInput from "react-phone-number-input";
+import PnoneLibInput from 'react-phone-number-input'
+import { getCountryCallingCode } from 'react-phone-number-input'
 
-import styles from "./styles.scss";
+import styles from './styles.scss'
+
 
 const Phone = (props) => {
-  const { placeholder, label, error, onChange, value } = props;
+  const { placeholder, label, error, onChange, value } = props
+  const _onChange = (phone) => {
+    if (!phone) {
+      try {
+        const selectedCountry = document.querySelector('.PhoneInputCountry SELECT').value
+        phone = `+${getCountryCallingCode(selectedCountry)} `
+        document.querySelector('.PhoneInputInput').value = phone
+      } catch (e) { }
+    } else {
+      onChange(phone)
+    }
+  }
   return (
     <div styleName="highLevel phone_inut" className="ym-hide-content">
       {label && label}
       <PnoneLibInput
         styleName="phoneNumber"
         value={value}
-        onChange={onChange}
+        onChange={_onChange}
         placeholder={placeholder}
       />
       {error && <div styleName="rednote">{error}</div>}
