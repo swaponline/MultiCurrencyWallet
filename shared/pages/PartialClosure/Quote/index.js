95c03b5feba6023fc467fd27cf0af77b7bc19005
uhaubuhau894@gmail.com <uhaubuhau894@gmail.com>
2020-06-27 12:21:04 +1000

    fix dark theme dark logo and some text colors

--- shared/pages/PartialClosure/Quote/index.js
+++ shared/pages/PartialClosure/Quote/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { FormattedMessage } from 'react-intl'
+import { constants } from 'helpers'
 
 import CSSModules from 'react-css-modules'
 import styles from './styles.scss'
 
+
+const isDark = localStorage.getItem(constants.localStorage.isDark)
 /* eslint-disable */
 const Quote = ({ }) => {
   const href = "https://blog.qtum.org/atomic-cross-chain-swap-on-qtum-7e756a890ed7"
   return (
-    <div styleName="container">
+    <div styleName={`container ${isDark ? 'dark' : ''}`}>
       <div styleName="header">
         &#34;
       <FormattedMessage
