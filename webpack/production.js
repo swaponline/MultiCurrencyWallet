1df2a2a92cc99665c1404ae7b4d94362092eddb0
shendel <im_shendel@inbox.ru>
2020-07-14 20:47:15 +0300

    Revert "disalow minify (temp)"

--- webpack/production.js
+++ webpack/production.js
@@ -36,7 +36,7 @@ export default (webpackConfig) => {
   })
 
   webpackConfig.plugins.push(
-    // new TerserPlugin(),
+    new TerserPlugin(),
     new WebpackRequireFrom({
       variableName: 'publicUrl',
       suppressErrors: true,
