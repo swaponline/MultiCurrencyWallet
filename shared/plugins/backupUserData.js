3c1f9fe8f43b7b7a74460cf9adb120a895e1658e
shendel <im_shendel@inbox.ru>
2020-07-31 13:27:38 +0300

    backup user data plugin (draft)

---
+++ shared/plugins/backupUserData.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+import { constants, localStorage } from 'helpers'
+import config from 'helpers/externalConfig'
+
+
+const backupUserData = {
+  isUserChanged: () => {
+  },
+  backupUser: () => {
+  },
+  restoreUser: () => {
+  }
+}
+
+export default backupUserData
