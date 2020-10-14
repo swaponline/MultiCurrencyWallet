bc7d5d1c3b44dbf5815be469dda14ea6b05e293f
shendel <im_shendel@inbox.ru>
2020-10-14 11:05:20 +0300

    fix react-router-dom

--- src/front/shared/containers/IntlProviderContainer.js
+++ src/front/shared/containers/IntlProviderContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { IntlProvider, addLocaleData } from 'react-intl'
-import { Switch } from 'react-router-dom'
-import { Route } from 'react-router'
+import { Switch, Route, BrowserRouter, HashRouter } from 'react-router-dom'
+//import { Route } from 'react-router'
 import localeEn from 'react-intl/locale-data/en'
 import localeRu from 'react-intl/locale-data/ru'
 import localeNl from 'react-intl/locale-data/nl'
@@ -33,29 +33,31 @@ export default class IntlProviderContainer extends Component {
     const { children } = this.props
     let lang = 'en'
     return (
-      <Switch>
-        <Route
-          path={localisePrefix}
-          render={props => {
-            let currentLocale = defaultLocale()
-            if (props.match.params.locale !== undefined) {
-              currentLocale = props.match.params.locale
-            } else {
-              lang = getCookie('mylang') || 'en'
-              currentLocale = lang.toLowerCase()
+      <HashRouter>
+        <Switch>
+          <Route
+            path={localisePrefix}
+            render={props => {
+              let currentLocale = defaultLocale()
+              if (props.match.params.locale !== undefined) {
+                currentLocale = props.match.params.locale
+              } else {
+                lang = getCookie('mylang') || 'en'
+                currentLocale = lang.toLowerCase()
 
-            }
+              }
 
-            const messages = translations[currentLocale]
+              const messages = translations[currentLocale]
 
-            return (
-              <IntlProvider {...props} key={currentLocale} locale={currentLocale} defaultLocale={defaultLocale()} messages={messages}>
-                {children}
-              </IntlProvider>
-            )
-          }}
-        />
-      </Switch>
+              return (
+                <IntlProvider {...props} key={currentLocale} locale={currentLocale} defaultLocale={defaultLocale()} messages={messages}>
+                  {children}
+                </IntlProvider>
+              )
+            }}
+          />
+        </Switch>
+      </HashRouter>
     )
   }
 }
