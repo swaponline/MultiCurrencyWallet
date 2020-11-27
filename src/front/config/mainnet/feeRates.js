fe28c404193348c9886dfc224ddbeea3708ea8d1
Orsoev Mikhail <n0tEternal@ya.ru>
2020-11-27 22:12:13 +0800

    Change mainnet fee link

--- src/front/config/mainnet/feeRates.js
+++ src/front/config/mainnet/feeRates.js
@@ -1,6 +1,10 @@
-const ethGasApiLink = window.defipulse_api_key
-  ? `https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key=${window.defipulse_api_key}`
-  : 'https://noxon.wpmix.net/ethGas.php'
+const ethGasApiLink = 'https://noxon.wpmix.net/ethGas.php'
+
+if (typeof window !== 'undefined') {
+  if (window.defipulse_api_key) {
+    ethGasApiLink = `https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key=${window.defipulse_api_key}`
+  }
+}
 
 export default {
   // eth: 'https://www.etherchain.org/api/gasPriceOracle',
--- src/front/shared/helpers/eth.ts
+++ src/front/shared/helpers/eth.ts
@@ -11,7 +11,7 @@ type EstimateFeeOptions = {
 const estimateFeeValue = async (options: EstimateFeeOptions) => {
   /* 
   * method -> send, swap
-  * speed -> safeLow, standard, fast, fastest
+  * speed -> slow, fast, fastest
   */
   const { method, speed } = options
   const gasPrice = await estimateGasPrice({ speed })
