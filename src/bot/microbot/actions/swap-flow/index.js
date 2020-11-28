5e9e3e1bc6672034a0dd5f3bf84b6e6393aefad4
shendel <im_shendel@inbox.ru>
2020-11-28 17:07:50 +0300

    js - ts

--- src/bot/microbot/actions/swap-flow/index.js
+++
@@ -1,4 +0,0 @@
-export BTC2ETHFlow from './BTC2ETHFlow'
-export ETH2BTCFlow from './ETH2BTCFlow'
-export UTXO2ETHFlow from './UTXO2ETHFlow'
-export ETH2UTXOFlow from './ETH2UTXOFlow'
--- src/bot/microbot/actions/swap-flow/index.ts
+++ src/bot/microbot/actions/swap-flow/index.ts
@@ -1,5 +1,8 @@
 import BTC2ETHFlow from './BTC2ETHFlow'
 import ETH2BTCFlow from './ETH2BTCFlow'
+import UTXO2ETHFlow from './UTXO2ETHFlow'
+import ETH2UTXOFlow from './ETH2UTXOFlow'
+
 
 export {
   BTC2ETHFlow,
