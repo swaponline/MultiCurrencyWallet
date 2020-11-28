ed01121a803ea760fc9827a74bec2315afd1be7d
shendel <im_shendel@inbox.ru>
2020-11-28 12:45:42 +0300

    ts-bot - fix module alias resolve

--- babel.bot.config.js
+++ babel.bot.config.js
@@ -8,6 +8,30 @@ module.exports = {
     '@babel/preset-typescript',
   ],
   'plugins': [
+    [
+      'module-resolver',
+      {
+          extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          root: ['./src'],
+          alias: {
+            "swap.app": "./src/core/swap.app/index.ts",
+            "swap.app/*": "./src/core/swap.app/*",
+            "swap.auth": "./src/core/swap.auth/index.ts",
+            "swap.auth/*": "./src/core/swap.auth/*",
+            "swap.flows": "./src/core/swap.flows/index.ts",
+            "swap.flows/*": "./src/core/swap.flows/*",
+            "swap.orders": "./src/core/swap.orders/index.ts",
+            "swap.orders/*": "./src/core/swap.orders/*",
+            "swap.room": "./src/core/swap.room/index.ts",
+            "swap.room/*": "./src/core/swap.room/*",
+            "swap.swap": "./src/core/swap.swap/index.ts",
+            "swap.swap/*": "./src/core/swap.swap/*",
+            "swap.swaps": "./src/core/swap.swaps/index.ts",
+            "swap.swaps/*": "./src/core/swap.swaps/*",
+            "simple.swap.core": "./src/core/simple/src/index.ts"
+          },
+      },
+    ],
     'add-module-exports',
     ['@babel/plugin-proposal-decorators', { 'legacy': true }],
     '@babel/plugin-proposal-class-properties',
