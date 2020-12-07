aaa53eb2a3878ec5b53ec930aaae755e6bea26f7
notEternal <n0tEternal@ya.ru>
2020-12-07 19:10:27 +0800

    Fix mistake in the names

--- src/common/coins/interfaces.ts
+++ src/common/coins/interfaces.ts
@@ -20,8 +20,8 @@ export interface IConnector {
   fetchBalance(networkType: string, address: string): Promise<number>
 
   fetchUnspents?(netType: string, addr: string): Promise<JSON>
-  fetchTx?(tsid: string): Promise<void> // this method is empty now
-  fetchRawTx?(tsid: string): Promise<void> // this method is empty now
+  fetchTx?(txid: string): Promise<void> // this method is empty now
+  fetchRawTx?(txid: string): Promise<void> // this method is empty now
   publishRawTx?(netType: string, rawTx: string): Promise<JSON>
   publishTx?(networkType: string, rawTx: string): Promise<JSON>
 }
