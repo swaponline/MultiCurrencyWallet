b13d557ac2acf67924a3c45e7be4f3621d7f05ef
iower <iower@tutanota.com>
2020-12-10 17:05:37 +0500

    ts3->4: fix slider (#3609)

--- src/front/shared/pages/Wallet/components/NotityBlock/NotifyBlock.tsx
+++ src/front/shared/pages/Wallet/components/NotityBlock/NotifyBlock.tsx
@@ -1,52 +1,65 @@
 import React from 'react'
-import { withRouter } from 'react-router-dom'
+import { withRouter, RouteComponentProps } from 'react-router-dom'
 import CSSModules from 'react-css-modules'
 import styles from './NotifyBlock.scss'
 import feedback from 'shared/helpers/feedback'
 
 
-const NotifyBlock = ({
-  icon,
-  descr,
-  tooltip,
-  firstFunc,
-  background,
-  link,
-  history,
-  logDescr,
-}) => {
-  const handleGoto = () => {
-    firstFunc && firstFunc()
+interface IProps {
+  className?: string
+  background: string
+  icon?: string
+  text: JSX.Element | string
+  feedbackText?: string
+  onPress?: () => void
+  link?: string
+}
+
+const NotifyBlock = (props: IProps & RouteComponentProps) => {
+  const {
+    className,
+    icon,
+    text,
+    onPress,
+    background,
+    link,
+    feedbackText,
+    history,
+  } = props
+
+  const handleClick = () => {
+    onPress && onPress()
+
     if (link && link.includes('http')) {
-      window.location = link
+      window.location.href = link
     } else {
       if (link) {
         history.push(link)
       }
     }
-    const text = logDescr || descr
-    //@ts-ignore
-    feedback.wallet.clickedBanner(text)
+
+    const textToSend = feedbackText || text
+    feedback.wallet.clickedBanner(textToSend)
   }
 
   const backGroundStyle = { background: `#${background}` }
   const backGroundImgStyle = { backgroundImage: `url(${background})` }
   const style = background && background.length < 7 ? backGroundStyle : backGroundImgStyle
+
   return (
-    <div styleName="notifyBlock" style={style} onClick={handleGoto}>
+    <div styleName="notifyBlock" style={style} onClick={handleClick}>
       {background && background.length > 7 ? <div styleName="notifyBlockOverlay" /> : ''}
       <div>
         <div styleName="notifyBlockIcon">
           <img src={icon} alt="" />
         </div>
         <div styleName="notifyBlockDescr">
-          <span>{descr}</span>
-          <span>{tooltip}</span>
+          <span>{text}</span>
         </div>
       </div>
     </div>
   )
 }
 
-//@ts-ignore
+
 export default withRouter(CSSModules(NotifyBlock, styles, { allowMultiple: true }))
