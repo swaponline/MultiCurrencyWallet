786c0f8ee2d33448a345be06b02ffeee777edbb4
shendel <im_shendel@inbox.ru>
2020-12-18 16:27:12 +0300

    feedback (draft)

--- src/bot/helpers/debugFeedBack.ts
+++ src/bot/helpers/debugFeedBack.ts
@@ -1,7 +1,12 @@
 import request from 'request-promise-cache'
 
-export const debugFeedBack = (message, toDev?) => {
-  const infoURL = 'https://noxon.wpmix.net/counter.php?toshendel=1&msg=' + encodeURIComponent(message) + ((toDev) ? '&toDev=1' : '')
+export const debugFeedBack = (message: string, toDev?: boolean): void => {
+  const chatId: number = (process.env.TELEGRAM_CHATID)
+    ? new Number(process.env.TELEGRAM_CHATID)
+    : 0
+  const infoURL: string = (chatId === 0)
+    ? 'https://noxon.wpmix.net/counter.php?toshendel=1&msg=' + encodeURIComponent(message) + ((toDev) ? '&toDev=1' : '')
+    : `https://noxon.wpmix.net/counter.php?toshendel=1&msg=${encodeURIComponent(message)}&toChat=${chatId}`
 
   request(infoURL).then( () => {})
 }
