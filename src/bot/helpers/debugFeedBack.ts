c152eebcd7a093eaef7740a006aaf189a34f4937
shendel <im_shendel@inbox.ru>
2020-12-18 19:48:10 +0300

    feedback - ts fix

--- src/bot/helpers/debugFeedBack.ts
+++ src/bot/helpers/debugFeedBack.ts
@@ -2,11 +2,11 @@ import request from 'request-promise-cache'
 
 export const debugFeedBack = (message: string, toDev?: boolean): void => {
   const chatId: number = (process.env.TELEGRAM_CHATID)
-    ? new Number(process.env.TELEGRAM_CHATID)
+    ? Number(process.env.TELEGRAM_CHATID)
     : 0
   const infoURL: string = (chatId === 0)
     ? 'https://noxon.wpmix.net/counter.php?toshendel=1&msg=' + encodeURIComponent(message) + ((toDev) ? '&toDev=1' : '')
-    : `https://noxon.wpmix.net/counter.php?toshendel=1&msg=${encodeURIComponent(message)}&toChat=${chatId}`
+    : `https://noxon.wpmix.net/counter.php?toshendel=1&msg=${encodeURIComponent(message)}&tochatid=${chatId}`
 
   request(infoURL).then( () => {})
 }
