4b388f36b1b9ecb547a495166e93e4dd08b871a2
Айрат <khasanofficial14@gmail.com>
2021-01-19 01:41:46 +0300

    run project and fix bugs

--- src/front/shared/components/layout/Center/Center.tsx
+++ src/front/shared/components/layout/Center/Center.tsx
@@ -5,34 +5,48 @@ import cx from 'classnames'
 import cssModules from 'react-css-modules'
 import styles from './Center.scss'
 
+interface CenterProps {
+  children: React.ReactNode
+  relative: boolean
+  scrollable: boolean
+  keepFontSize: boolean
+  centerVertically: boolean
+  centerHorizontally: boolean
+}
 
-const Center = ({ children, scrollable, centerHorizontally, centerVertically, keepFontSize, relative, ...rest }) => {
+const Center = ({
+  children,
+  scrollable,
+  centerHorizontally,
+  centerVertically,
+  keepFontSize,
+  relative,
+  ...rest
+}: CenterProps) => {
   // TODO move overflow to Modal and any other cases where it belongs
   const styleName = cx('centringContainer', {
-    'scrollable': scrollable,
-    'centerHorizontally': centerHorizontally,
-    'centerVertically': centerVertically,
-    'keepFontSize': keepFontSize,
-    'relative': relative,
+    scrollable: scrollable,
+    centerHorizontally: centerHorizontally,
+    centerVertically: centerVertically,
+    keepFontSize: keepFontSize,
+    relative: relative,
   })
 
   return (
     <div styleName={styleName} {...rest}>
-      <div styleName="centringContent">
-        {children}
-      </div>
+      <div styleName="centringContent">{children}</div>
     </div>
   )
 }
 
-Center.propTypes = {
-  relative: PropTypes.bool,
-  children: PropTypes.node,
-  scrollable: PropTypes.bool,
-  keepFontSize: PropTypes.bool,
-  centerVertically: PropTypes.bool,
-  centerHorizontally: PropTypes.bool,
-}
+// Center.propTypes = {
+//   relative: PropTypes.bool,
+//   children: PropTypes.node,
+//   scrollable: PropTypes.bool,
+//   keepFontSize: PropTypes.bool,
+//   centerVertically: PropTypes.bool,
+//   centerHorizontally: PropTypes.bool,
+// }
 
 Center.defaultProps = {
   relative: false,
@@ -41,6 +55,5 @@ Center.defaultProps = {
   centerVertically: true,
   centerHorizontally: true,
 }
-
 
 export default cssModules(Center, styles, { allowMultiple: true })
