1ed30088f00a7631134212b082f05d6ddf4fa723
NotEternal <cent1pede@protonmail.ch>
2021-02-24 09:29:37 -0800

    Update plugin scripts

--- scripts/updateFarmPlugin.sh
+++ scripts/updateFarmPlugin.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 
-farmPagePath="../src/front/shared/pages/FarmFactory/lib/"
+echo "Enter token for farmdeployer.js (ex. E1HOSDK31...)"
+read deployerToken
 
-curl -O "https://raw.githubusercontent.com/noxonsu/farmfactory/main/lib/farmdeployer.js"
-curl -O "https://raw.githubusercontent.com/noxonsu/farmfactory/main/lib/farmfactory.js"
-curl -O "https://raw.githubusercontent.com/noxonsu/farmfactory/main/farmfactory.css"
+echo "Enter token for farmfactory.js (ex. E1HOSDK31...)"
+read factoryToken
 
-# checking that all files was downloaded
-if [[ -e farmdeployer.js && -e farmfactory.js && -e farmfactory.css ]]
+echo "Enter token for farmfactory.css (ex. E1HOSDK31...)"
+read factoryStylesToken
+
+farmPagePath="./src/front/shared/pages/FarmFactory/lib/"
+
+
+
+# checking that all tokens aren't empty
+if [[ -n "$deployerToken" && -n "$factoryToken" && -n "$factoryStylesToken"  ]]
 then
-  # move files to the farm page directory
-  mv farmdeployer.js "$farmPagePath"
-  mv farmfactory.js "$farmPagePath"
-  mv farmfactory.css "$farmPagePath"
+  curl -o farmdeployer.js "https://raw.githubusercontent.com/noxonsu/farmfactory/main/lib/farmdeployer.js?token=$deployerToken"
+  curl -o farmfactory.js "https://raw.githubusercontent.com/noxonsu/farmfactory/main/lib/farmfactory.js?token=$factoryToken"
+  curl -o farmfactory.css "https://raw.githubusercontent.com/noxonsu/farmfactory/main/farmfactory.css?token=$factoryStylesToken"
 
-  cd "$farmPagePath"
+  # checking that all files was downloaded
+  if [[ -s farmdeployer.js && -s farmfactory.js && -s farmfactory.css ]]
+  then
+    # move files to the farm page directory
+    mv farmdeployer.js "$farmPagePath"
+    mv farmfactory.js "$farmPagePath"
+    mv farmfactory.css "$farmPagePath"
 
-  # add 'export' in the beginning of the files
-  sed -i "1s/^/export /" farmdeployer.js
-  sed -i "1s/^/export /" farmfactory.js
-else
-  echo "Can not download files"
+    cd "$farmPagePath"
+
+    # add 'export' in the beginning of the files
+    sed -i "1s/^/export /" farmdeployer.js
+    sed -i "1s/^/export /" farmfactory.js
+  else
+    rm farmdeployer.js farmfactory.js farmfactory.css
+    echo "Cannot download files"
+  fi
+else 
+  echo "Not all tokens are listed"
 fi
-
