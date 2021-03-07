e999de3bb80c3369cebfa559bba6cec674188414
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-03-07 14:46:13 +0600

    add constant bitcoin fees

--- src/front/shared/components/modals/WithdrawModal/components/FeeInfoBlock/FeeRadios/index.tsx
+++ src/front/shared/components/modals/WithdrawModal/components/FeeInfoBlock/FeeRadios/index.tsx
@@ -7,6 +7,45 @@ type FeeRadiosState = {
     feeValue: number
 }
 
+const bitcoinFees = [
+    {
+        id: 1,
+        time: '~30-60 minutes',
+        title: 'Slow',
+        slug: 'hourFee',
+        speedType: 'slow',
+        value: 5 * 1e3,
+        description: 'The lowest fee (in satoshis per byte) that will confirm transactions within an hour (with 90% probability)'
+    },
+    {
+        id: 2,
+        time: '~15-30 minutes',
+        title: 'Medium',
+        slug: 'halfHourFee',
+        speedType: 'medium',
+        value: 15 * 1e3,
+        description: 'The lowest fee (in satoshis per byte) that will confirm transactions within half an hour (with 90% probability)'
+    },
+    {
+        id: 3,
+        time: '~0-15 minutes',
+        title: 'Fast',
+        slug: 'fastestFee',
+        speedType: 'fast',
+        value: 30 * 1e3,
+        description: 'The lowest fee (in satoshis per byte) that will currently result in the fastest transaction confirmations (usually 0 to 1 block delay)'
+    },
+    {
+        id: 4,
+        time: '',
+        title: 'Custom',
+        slug: 'customFee',
+        speedType: 'custom',
+        value: 1,
+        description: 'Set custom fee rate'
+    },
+]
+
 @cssModules(styles, { allowMultiple: true })
 export default class FeeRadios extends Component<{}, FeeRadiosState> {
     constructor(props) {
