43893d47efea2901307ae6fe9fbe3e390a08ba8d
NotEternal <cent1pede@protonmail.ch>
2021-03-10 03:39:15 -0800

    App component: delete useless code

--- src/front/shared/containers/App/App.tsx
+++ src/front/shared/containers/App/App.tsx
@@ -275,22 +275,6 @@ class App extends React.Component<RouteComponentProps<any>, any> {
       }
     }
 
-    // @ToDo - may be can be deleted. Temp fix for our client, when he update token list
-    if (window.origin === `https://wallet.b` + `itpli` + `cit` + `y.com`) {
-      const tokenListUpdated = localStorage.getItem('widget_tokenupdated')
-      if (!tokenListUpdated) {
-        localStorage.setItem('widget_tokenupdated', true)
-        Object.keys(config.erc20).forEach((tokenCode) => {
-          if ((tokenCode !== `bitpl`)
-            && (tokenCode !== `usdt`)
-          ) {
-            console.log('Hide', tokenCode)
-            actions.core.markCoinAsHidden(tokenCode.toUpperCase())
-          }
-        })
-      }
-    }
-
     const isWalletCreate = localStorage.getItem(constants.localStorage.isWalletCreate)
 
     if (!isWalletCreate) {
