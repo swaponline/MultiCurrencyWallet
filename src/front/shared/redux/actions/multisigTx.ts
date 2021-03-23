357c02cd5b6ae03895f8ac780626daf8f8490752
Gercog52 <54477063+Gercog52@users.noreply.github.com>
2021-03-24 05:37:53 +0800

    add types

--- src/front/shared/redux/actions/multisigTx.ts
+++ src/front/shared/redux/actions/multisigTx.ts
@@ -84,7 +84,8 @@ const fetch = (address) => {
   const { user: { btcData } } = getState()
   const holderKey = btcData.publicKey.toString('hex')
 
-  let firstPending = false
+
+  let firstPending: {address: any, item: any} | boolean = false
   let pengingCount = 0
 
   return apiLooper.post('multisig', `/txs/`, {
@@ -114,7 +115,6 @@ const fetch = (address) => {
 
         // @ToDo - (draft) use api request for fetch status of address list
         if (status === 'pending' && holderKey !== item.holder) {
-          //@ts-ignore
           firstPending = firstPending || {
             address,
             item,
