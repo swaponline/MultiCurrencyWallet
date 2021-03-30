fa66511486c2ffad544f4ffd9c3acc02dbd1ca22
iower <iower@tutanota.com>
2021-03-30 13:28:43 +0500

    MM promo page: FAQ as component (#4164)

--- src/front/shared/pages/Marketmaker/Marketmaker.tsx
+++ src/front/shared/pages/Marketmaker/Marketmaker.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import cssModules from 'react-css-modules'
 import { FormattedMessage } from 'react-intl'
+
 import styles from './Marketmaker.scss'
 import { feedback } from 'helpers'
 import Button from 'components/controls/Button/Button'
 import Expandable from 'components/ui/Expandable/Expandable'
+import FAQ from './FAQ'
 
 
 @cssModules(styles, { allowMultiple: true })
-export default class Marketmaker extends React.Component<null, null> {
+export default class Marketmaker extends React.Component<{}, {}> {
   constructor(props) {
     super(props)
   }
 
   componentDidMount() {
-    //feedback.marketmaker.entered()
+    //feedback.marketmakerPromo.entered()
   }
 
   onSelectBrowser() {
-    //feedback.marketmaker.selected('browser')
+    //feedback.marketmakerPromo.selected('browser')
   }
 
   onSelectServer() {
@@ -55,41 +57,9 @@ export default class Marketmaker extends React.Component<null, null> {
             </div>
           </div>
         </section>
-        <section styleName="faq">
-          <h2 styleName="section-title">FAQ</h2>
-          <Expandable
-            title="Почему это работает?"
-            content="Маркетмейкер зарабатывает на разности курсов при обслуживании покупки и продажи (спред)"
-          />
-          <Expandable
-            title="Это безопасно?"
-            content={
-              <>
-                <div>- Система находится в бета версии</div>
-                <div>- Есть аудит от dsec</div>
-                <div>- Рекомендуем инвестировать только те средства, которые не жалко потерять</div>
-                <div>- Ключи от ваших средств находятся только у вас, мы не имеем доступа к вашим средствам</div>
-                <div>- <a href="https://github.com/swaponline/MultiCurrencyWallet/blob/master/docs/RISKS.md" target="_blank">Уведомление о рисках</a></div>
-              </>
-            }
-          />
-          <Expandable
-            title="Сколько я заработаю?"
-            content={<div>Зависит от количества обменов и спреда. Но если обменов не будет, вы получите SWAP токены из расчета 10% APY. Для получения напишите в <a href="https://t.me/swaponlinebot" target="_blank">саппорт</a>.</div>}
-          />
-          <Expandable
-            title="Какой минимальный баланс нужен для начала?"
-            content="(в разработке)"
-          />
-          <Expandable
-            title="Какой минимальный срок маркетмейкинга?"
-            content="Минимального срока нет, вы можете вывести средства в любой момент."
-          />
-          <Expandable
-            title="Как увеличить заработок?"
-            content="Расскажите о возможности обмена в сообществах где вы состоите."
-          />
-        </section>
+
+        <FAQ />
+
         <section styleName="marketmaker-settings">
           <h2 styleName="section-title">Настройки маркетмейкинга</h2>
   {/*        Маркетмейкинг BTC/WBTC : вкл/выкл
