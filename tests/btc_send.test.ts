b07a7cc970eeb3c9fb1c940992e414c67b61ee30
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-04-06 12:26:39 +0600

    add test 'regular пробует отправить 1000 sat'

--- tests/btc_send.test.ts
+++ tests/btc_send.test.ts
@@ -1,17 +1,6 @@
 import helpers from  '../src/front/shared/helpers'
 import btcActions from "../src/front/shared/redux/actions/btc";
-import config from '../src/front//local_modules/app-config'
-
-const hasAdminFee = (config
-  && config.opts
-  && config.opts.fee
-  && config.opts.fee.btc
-  && config.opts.fee.btc.fee
-  && config.opts.fee.btc.address
-  && config.opts.fee.btc.min
-) ? config.opts.fee.btc : false;
-
-const NETWORK = "TESTNET"
+import BigNumber from 'bignumber.js';
 
 test('calculate txSize of 1 txIn, 2 txOut and method send for P2PKH address', async () => {
   const params = {
@@ -26,15 +15,27 @@ test('calculate txSize of 1 txIn, 2 txOut and method send for P2PKH address', as
   expect(txSize).toBe(230);
 });
 
-test('send regular transaction', async () => {
+test('send regular transaction with 1000 satoshis', async () => {
+  await btcActions.login("cS3TJmYMW1QnnuFV31iyuP6W22LM8oF9VDbGC5j3Hx2ieiZtVzVi");
   const options = {
     from: 'mjCrCbTP5UqzDCSN86uGqBfJCgYBcbCmuy',
     to: 'mwcbYjxbizC5ejSrfjUhjuiT56vQCTqLmY',
-    amount: 1e3,
-    feeValue: null,
+    amount: 1e-5,
+    feeValue: new BigNumber(1e-5),
     speed: "fast"
   };
 
-  const txHash = btcActions.send(options)
-  const txInfo = btcActions.fetchTxInfo(txHash, 10000)
+  const txHash = await btcActions.send(options);
+  const {
+    amount,
+    senderAddress,
+    receiverAddress,
+
+    minerFee,
+    adminFee,
+    minerFeeCurrency,
+
+    size
+  } = await btcActions.fetchTxInfo(txHash, 10000);
+  expect(amount).toBe(options.amount);
 })
