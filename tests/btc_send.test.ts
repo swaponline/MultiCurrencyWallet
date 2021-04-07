edd02b5f7a7d1412597e29d7ce433bdd57e8bcca
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-04-07 11:00:49 +0600

    add timeOut between send and fetchTxInfo and refactoring tests

--- tests/btc_send.test.ts
+++ tests/btc_send.test.ts
@@ -2,79 +2,85 @@ import helpers from  '../src/front/shared/helpers'
 import btcActions from "../src/front/shared/redux/actions/btc";
 import BigNumber from 'bignumber.js';
 
-test('calculate txSize of 1 txIn, 2 txOut and method send for P2PKH address', async () => {
-  const params = {
-    txIn: 1,
-    txOut: 2,
-    method: 'send',
-    fixed: false,
-    toAddress: 'mjCrCbTP5UqzDCSN86uGqBfJCgYBcbCmuy'
-  };
+const timeOut = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
-  const txSize = await helpers.btc.calculateTxSize(params);
-  expect(txSize).toBe(230);
-});
+describe('BTC Send Tests', () => {
+  it('calculate txSize of 1 txIn, 2 txOut and method send for P2PKH address', async () => {
+    const params = {
+      txIn: 1,
+      txOut: 2,
+      method: 'send',
+      fixed: false,
+      toAddress: 'mjCrCbTP5UqzDCSN86uGqBfJCgYBcbCmuy'
+    };
 
-test('send regular transaction with 1000 satoshis', async () => {
-  await btcActions.login("cS3TJmYMW1QnnuFV31iyuP6W22LM8oF9VDbGC5j3Hx2ieiZtVzVi");
-  const options = {
-    from: 'mjCrCbTP5UqzDCSN86uGqBfJCgYBcbCmuy',
-    to: 'mwcbYjxbizC5ejSrfjUhjuiT56vQCTqLmY',
-    amount: 1e-5,
-    feeValue: new BigNumber(1e-5),
-    speed: "fast"
-  };
+    const txSize = await helpers.btc.calculateTxSize(params);
+    expect(txSize).toBe(230);
+  }, 2000);
 
-  const txHash = await btcActions.send(options);
-  const {
-    amount,
-    senderAddress,
-    receiverAddress,
+  it('send regular transaction with 1000 satoshis', async () => {
+    await btcActions.login("cS3TJmYMW1QnnuFV31iyuP6W22LM8oF9VDbGC5j3Hx2ieiZtVzVi");
+    const options = {
+      from: 'mjCrCbTP5UqzDCSN86uGqBfJCgYBcbCmuy',
+      to: 'mmorWg8tkUmwudQ2cvn7GC3PwE5XgDaBbn',
+      amount: 1e-5,
+      feeValue: new BigNumber(1e-5),
+      speed: "fast"
+    };
 
-    minerFee,
-    adminFee,
-    minerFeeCurrency,
+    const txHash = await btcActions.send(options);
+    await timeOut(5 * 1000)
+    const {
+      amount,
+      senderAddress,
+      receiverAddress,
 
-    size
-  } = await btcActions.fetchTxInfo(txHash, 10000);
-  expect(amount).toBe(options.amount);
-})
+      minerFee,
+      adminFee,
+      minerFeeCurrency,
 
-test('send regular transaction with 1000 satoshis with adminFee', async () => {
-  const serviceFee = {
-    fee: '5',
-    address: '2MuXz9BErMbWmoTshGgkjd7aMHeaxV8Bdkk',
-    min: '0.00001',
-  }
+      size
+    } = await btcActions.fetchTxInfo(txHash, 8000);
+    expect(amount).toBe(options.amount);
+  }, 20000)
 
-  const options = {
-    from: 'mwcbYjxbizC5ejSrfjUhjuiT56vQCTqLmY',
-    to: 'mjCrCbTP5UqzDCSN86uGqBfJCgYBcbCmuy',
-    amount: 1e-5,
-    feeValue: new BigNumber(1e-5),
-    speed: "fast",
-    serviceFee
-  };
+  it('send regular transaction with 1000 satoshis with adminFee', async () => {
+    const serviceFee = {
+      fee: '5',
+      address: '2MuXz9BErMbWmoTshGgkjd7aMHeaxV8Bdkk',
+      min: '0.00001',
+    }
 
-  const adminFeeMin = new BigNumber(serviceFee.min);
-  let feeFromAmount = new BigNumber(serviceFee.fee).dividedBy(100).multipliedBy(options.amount);
-  if (adminFeeMin.isGreaterThan(feeFromAmount)) feeFromAmount = adminFeeMin;
+    const options = {
+      from: 'mwcbYjxbizC5ejSrfjUhjuiT56vQCTqLmY',
+      to: 'mpb5DPTbz6Mu9RkvS1ctMRQeZc7r3GnLdn',
+      amount: 1e-5,
+      feeValue: new BigNumber(1e-5),
+      speed: "fast",
+      serviceFee
+    };
 
-  await btcActions.login("cR2QGm1SLqmvgYBUtroVmVaBRKSSsbAAeqQ54YTA4xXELCcyoWtL");
+    const adminFeeMin = new BigNumber(serviceFee.min);
+    let feeFromAmount = new BigNumber(serviceFee.fee).dividedBy(100).multipliedBy(options.amount);
+    if (adminFeeMin.isGreaterThan(feeFromAmount)) feeFromAmount = adminFeeMin;
 
-  const txHash = await btcActions.send(options);
-  const {
-    amount,
-    senderAddress,
-    receiverAddress,
+    await btcActions.login("cR2QGm1SLqmvgYBUtroVmVaBRKSSsbAAeqQ54YTA4xXELCcyoWtL");
 
-    minerFee,
-    adminFee,
-    minerFeeCurrency,
+    const txHash = await btcActions.send(options);
+    await timeOut(5 * 1000)
+    const {
+      amount,
+      senderAddress,
+      receiverAddress,
 
-    size
-  } = await btcActions.fetchTxInfo(txHash, 10000, serviceFee);
+      minerFee,
+      adminFee,
+      minerFeeCurrency,
 
-  expect(amount).toBe(options.amount);
-  expect(adminFee).toBe(feeFromAmount.toNumber());
+      size
+    } = await btcActions.fetchTxInfo(txHash, 8000, serviceFee);
+
+    expect(amount).toBe(options.amount);
+    expect(adminFee).toBe(feeFromAmount.toNumber());
+  }, 20000)
 })
