35a543b519a95551df4b7fbd0e2094ed5e210f32
Кудрявцев Дмитрий Вальтерович <mitya-kudr@mail.ru>
2021-04-25 18:53:09 +0300

    add sentry

--- src/front/client/index.tsx
+++ src/front/client/index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from 'shared/routes'
 import store, { history } from 'redux/store'
+import * as Sentry from '@sentry/react';
+import { Integrations } from "@sentry/tracing";
 
 import Root from 'shared/containers/Root/Root'
 import { migrate } from 'helpers'
@@ -18,15 +20,34 @@ const __webpack_public_path__ = `${config.publicPath}images/` // It makes webpac
 
 const rootEl = document.getElementById('root')
 
+if (process.env.NODE_ENV !== 'development') {
+  Sentry.init({
+    dsn: 'https://4d903dcd28e24737b28b3b381f6f58e5@o552924.ingest.sentry.io/5679412',
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+  });
+}
+
 if (
   window.location.protocol === 'http:' &&
   window.location.hostname !== 'localhost' &&
   !isLocalIP(window.location.hostname)
 ) {
-  ReactDOM.render(<ErrorPageNoSSL />, rootEl)
+  ReactDOM.render(
+    <Sentry.ErrorBoundary>
+      <ErrorPageNoSSL />
+    </Sentry.ErrorBoundary>
+    ,
+    rootEl
+  )
 } else {
   migrate().finally(() => setTimeout(() => {
-    ReactDOM.render(<Root history={history} store={store} routes={routes} />, rootEl)
+    ReactDOM.render(
+      <Sentry.ErrorBoundary>
+        <Root history={history} store={store} routes={routes} />
+      </Sentry.ErrorBoundary>,
+      rootEl
+    )
     console.log('(index.tsx) render root, finished')
   }, 1000))
 }
