cddb67b19cf1ae0dc5220fa4c238ed35b401a73f
shendel <im_shendel@inbox.ru>
2021-05-08 00:11:48 +0300

    avatar - gravatr support

--- src/front/shared/components/Avatar/Avatar.tsx
+++ src/front/shared/components/Avatar/Avatar.tsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
+import config from 'helpers/externalConfig'
 
 import { toSvg } from 'jdenticon'
 
@@ -7,17 +8,27 @@ import CSSModules from 'react-css-modules'
 import styles from './Avatar.scss'
 
 
-const Avatar = ({ value, className, size }) => (
-  <Fragment>
-    <img
-      className={className}
-      styleName="avatar"
-      alt={value}
-      title={value}
-      src={`data:image/svg+xml,${encodeURIComponent(toSvg(value, size))}`}
-    />
-  </Fragment>
-)
+const Avatar = ({ value, className, size, ownerEthAddress }) => {
+  let avatarUrl = `data:image/svg+xml,${encodeURIComponent(toSvg(value, size))}`
+  if (ownerEthAddress
+    && config
+    && config.gravatarUsers
+    && config.gravatarUsers[ownerEthAddress]
+  ) {
+    avatarUrl = `https://www.gravatar.com/avatar/${config.gravatarUsers[ownerEthAddress]}?s=${size}`
+  }
+  return (
+    <Fragment>
+      <img
+        className={className}
+        styleName="avatar"
+        alt={value}
+        title={value}
+        src={avatarUrl}
+      />
+    </Fragment>
+  )
+}
 
 Avatar.defaultProps = {
   size: 35,
@@ -27,6 +38,7 @@ Avatar.propTypes = {
   value: PropTypes.string.isRequired,
   size: PropTypes.number,
   className: PropTypes.string,
+  ownerEthAddress: PropTypes.string,
 }
 
 export default CSSModules(Avatar, styles)
