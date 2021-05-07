a2ffae0afc40894fd4204b624e85fb73bf6631e9
shendel <im_shendel@inbox.ru>
2021-05-08 00:24:00 +0300

    add gravatars

--- src/front/config/gravatarUsers.js
+++ src/front/config/gravatarUsers.js
@@ -2,6 +2,8 @@ const gravatarUsers = {
   // 'EthWallet': 'md5(gravatarEmail)'
   '0x276747801B0dbb7ba04685BA27102F1B27Ca0815': 'f4e5a577b92be59b96d4b0e3d85720a7', // eneeseene
   '0x7594E0bC1EB3592E773E29F5d9ec64e8a7F35789': 'f4e5a577b92be59b96d4b0e3d85720a7', // eneeseene
+  '0x04cC783c51fefd7f7DEc194017BBe78f16Ab9140': 'f4e5a577b92be59b96d4b0e3d85720a7', // eneeseene - testnet #1
+  '0x3B85D38c3A7AEabABA8B7DEb7a73177688270abC': '3acadd9952a49c013f40321bd2cc04f8', // noxon
 }
 
 
