43d9f1cc02e6f1ca2a77c016fa45cf4f1916b6e8
NotEternal <cent1pede@protonmail.ch>
2021-05-20 07:01:12 -0500

    Replace explorer names in the feeRates with variables

--- src/front/config/mainnet/feeRates.js
+++ src/front/config/mainnet/feeRates.js
@@ -1,7 +1,7 @@
 import api from './api'
 
 export default {
-  eth: `https://api.etherscan.io/api?module=proxy&action=eth_gasPrice&apikey=${api.etherscan_ApiKey}`,
-  bsc: `https://api.bscscan.com/api?module=proxy&action=eth_gasPrice&apikey=${api.bscscan_ApiKey}`,
+  eth: `${api.etherscan}?module=proxy&action=eth_gasPrice&apikey=${api.etherscan_ApiKey}`,
+  bsc: `${api.bscscan}?module=proxy&action=eth_gasPrice&apikey=${api.bscscan_ApiKey}`,
   btc: 'https://wiki.swaponline.io/blockcyper.php',
 }
