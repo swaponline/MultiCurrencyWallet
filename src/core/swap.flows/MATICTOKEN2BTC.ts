4e07d971c69e0d30e3b4fba0a3eb0a7577bc7f57
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-06-04 11:31:31 +0600

    add MATIC2BTC, MATICTOKEN2BTC and vice versa flows

---
+++ src/core/swap.flows/MATICTOKEN2BTC.ts
@@ -0,0 +1,33 @@
+import { constants } from 'swap.app'
+import EthLikeTokenToBtc from './atomic/EthLikeTokenToBtc'
+
+
+export default (tokenName) => {
+
+  class MATICTOKEN2BTC extends EthLikeTokenToBtc {
+    static blockchainName = `MATIC`
+    _flowName: string
+
+    static getName() {
+      return `{${this.blockchainName}}${this.getFromName()}2${this.getToName()}`
+    }
+    static getFromName() {
+      return tokenName.toUpperCase()
+    }
+    static getToName() {
+      return constants.COINS.btc
+    }
+
+    constructor(swap) {
+      super(swap, {
+        blockchainName: `MATIC`,
+        tokenName,
+        getMyAddress: swap.app.getMyMaticAddress.bind(swap.app),
+        getParticipantAddress: swap.app.getParticipantMaticAddress.bind(swap.app),
+      })
+      this._flowName = MATICTOKEN2BTC.getName()
+    }
+  }
+
+  return MATICTOKEN2BTC
+}
