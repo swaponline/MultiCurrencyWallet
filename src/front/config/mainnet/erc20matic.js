d4ab18c1af71aae019d739b88531a257d3e5ad53
NotEternal <cent1pede@protonmail.ch>
2021-06-07 05:16:42 -0500

    Update a mainnet config

--- src/front/config/mainnet/erc20matic.js
+++ src/front/config/mainnet/erc20matic.js
@@ -1,10 +1,4 @@
 export default {
-  weenus: {
-    address: '',
-    decimals: 18,
-    fullName: 'Weenus',
-    canSwap: true,
-  },
   wbtc: {
     address: '0x1bfd67037b42cf73acf2047067bd4f2c47d9bfd6',
     decimals: 8,
