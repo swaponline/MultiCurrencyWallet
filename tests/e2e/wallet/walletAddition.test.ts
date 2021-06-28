20b99ebedde92c3a9fd9a50351a51bb8ab981d1f
NotEternal <cent1pede@protonmail.ch>
2021-06-28 06:24:08 -0500

    Complete addition tests. Better renaming

--- tests/e2e/wallet/walletAddition.test.ts
+++ tests/e2e/wallet/walletAddition.test.ts
@@ -12,7 +12,21 @@ jest.setTimeout(250 * 1000)
 describe('Displaying wallets', () => {
   let testBrowser: puppeteer.Browser | undefined = undefined
   let testPage: puppeteer.Page | undefined = undefined
-  const wallets = ['eth', 'bnb', 'matic']
+  const wallets = [
+    ['eth', 'Ethereum'],
+    ['bnb', 'Binance Coin'],
+    ['matic', 'MATIC Token'],
+  ]
+
+  async function checkWalletDisplay(params) {
+    const { page, name, expectedTitle } = params
+
+    const walletTitle = await page.$eval(`#${name}WalletTitle`, el => el.textContent)
+
+    if (walletTitle !== expectedTitle) {
+      throw new Error('incorrect display for ${}')
+    }
+  }
 
   beforeAll(async () => {
     const { browser, page } = await createBrowser()
@@ -23,7 +37,7 @@ describe('Displaying wallets', () => {
     await page.waitForSelector('#preloaderCreateBtn')
     await page.click('#preloaderCreateBtn')
 
-    timeOut(60_000)
+    await timeOut(60_000)
   })
 
   afterAll(async () => {
@@ -49,19 +63,27 @@ describe('Displaying wallets', () => {
         selector: '#createWalletBtn',
       })
 
-      // check wallet display
+      await checkWalletDisplay({
+        page: testPage,
+        name: 'btc',
+        expectedTitle: 'Bitcoin'
+      })
     } else {
       throw new Error('page is not found')
     }
   })
 
-  it.each(wallets)('adding %s wallet', async (walletName) => {
+  it.each(wallets)('adding %s wallet', async (walletName, walletTitle) => {
     if (testPage) {
       await addAssetToWallet(testPage, walletName)
 
-      // check wallet display
+      await checkWalletDisplay({
+        page: testPage,
+        name: walletName,
+        expectedTitle: walletTitle,
+      })
 
-      takeScreenshot(testPage, `Add${walletName}Wallet`)
+      await takeScreenshot(testPage, `Add${walletName}Wallet`)
     } else {
       throw new Error('page is not found')
     }
