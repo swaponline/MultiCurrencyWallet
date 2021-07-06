0600a5b6deb7c5acc9a19f7a24642eb965de27cb
NotEternal <cent1pede@protonmail.ch>
2021-07-06 11:11:26 -0500

    Delete prop-types: second part

--- src/front/shared/components/controls/WithdrawButton/WithdrawButton.tsx
+++ src/front/shared/components/controls/WithdrawButton/WithdrawButton.tsx
@@ -1,15 +1,15 @@
-import React, { Fragment } from 'react'
-import PropTypes from 'prop-types'
-import config from 'app-config'
+import React from 'react'
 import cx from 'classnames'
-
 import CSSModules from 'react-css-modules'
 import styles from './WithdrawButton.scss'
-import ReactTooltip from 'react-tooltip'
-import { FormattedMessage } from 'react-intl'
 
+type ComponentProps = {
+  onClick: () => void
+  disable: boolean
+  children: JSX.Element | JSX.Element[]
+}
 
-const WithdrawButton = (props) => {
+const WithdrawButton = (props: ComponentProps) => {
   const {
     onClick,
     children,
@@ -17,21 +17,17 @@ const WithdrawButton = (props) => {
     ...rest
   } = props
 
+  const doNothing = () => undefined
+
   const styleName = cx('withdrawButton', {
     'disable': disable,
   })
 
   return (
-    <button onClick={!disable ? onClick : () => {}} styleName={styleName} {...rest}>
+    <button onClick={!disable ? onClick : doNothing} styleName={styleName} {...rest}>
       {children}
     </button>
   )
-  
-  //@ts-ignore
-  WithdrawButton.propTypes = {
-    onClick: PropTypes.func.isRequired,
-    className: PropTypes.string,
-    disabled: PropTypes.bool,
-  }
 }
+
 export default CSSModules(WithdrawButton, styles, { allowMultiple: true })
