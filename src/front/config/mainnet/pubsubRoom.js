beae0b09d6f0c190729232074cd7a04fbaea4438
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-07-21 21:31:15 +0600

    add public wrtc-star2

--- src/front/config/mainnet/pubsubRoom.js
+++ src/front/config/mainnet/pubsubRoom.js
@@ -1,3 +1,4 @@
 export default {
-  swarm: '/dns4/webrtc-star-1.swaponline.io/tcp/443/wss/p2p-webrtc-star/',
+  swarm: '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
+  // swarm: '/dns4/webrtc-star-1.swaponline.io/tcp/443/wss/p2p-webrtc-star/',
 }
