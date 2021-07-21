cb1e28dd9df5d21902af5825676ba304a39e0873
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-07-21 21:49:18 +0600

    set our Swarm

--- src/front/config/mainnet/pubsubRoom.js
+++ src/front/config/mainnet/pubsubRoom.js
@@ -1,4 +1,3 @@
 export default {
-  swarm: '/dns4/wrtc-star2.sjc.dwebops.pub/tcp/443/wss/p2p-webrtc-star/',
-  // swarm: '/dns4/webrtc-star-1.swaponline.io/tcp/443/wss/p2p-webrtc-star/',
+  swarm: '/dns4/webrtc-star-1.swaponline.io/tcp/443/wss/p2p-webrtc-star/',
 }
