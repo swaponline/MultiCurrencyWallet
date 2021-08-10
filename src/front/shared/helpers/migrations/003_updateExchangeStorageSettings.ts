5de0dbb993a3bdcad8c93bbefa47d3656ed70b39
NotEternal <cent1pede@protonmail.ch>
2021-08-10 02:35:26 -0500

    Complete third migration

--- src/front/shared/helpers/migrations/003_updateExchangeStorageSettings.ts
+++ src/front/shared/helpers/migrations/003_updateExchangeStorageSettings.ts
@@ -1,15 +1,15 @@
 import { localStorage, constants } from 'helpers'
 
-const name = 'Update exchange localStorage settings - convert settings from string into object'
+const name = 'Update exchange localStorage settings - remove extra string layer'
 
 const run = () => {
-/*   let exchangeSettings = localStorage.getItem(constants.localStorage.exchangeSettings)
+  let exchangeSettings = localStorage.getItem(constants.localStorage.exchangeSettings)
 
   if (typeof exchangeSettings === 'string') {
     exchangeSettings = JSON.parse(exchangeSettings)
-  }
 
-  localStorage.setItem(constants.localStorage.exchangeSettings, exchangeSettings) */
+    localStorage.setItem(constants.localStorage.exchangeSettings, exchangeSettings)
+  }
 
   return Promise.resolve()
 }
