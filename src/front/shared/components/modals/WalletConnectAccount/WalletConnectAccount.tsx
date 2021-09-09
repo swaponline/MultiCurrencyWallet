3a0b6e40d67b84ad545b9fdf0217d671b9ee6506
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-09-09 12:13:07 +0600

    add handle connect if disconneted in popup

--- src/front/shared/components/modals/WalletConnectAccount/WalletConnectAccount.tsx
+++ src/front/shared/components/modals/WalletConnectAccount/WalletConnectAccount.tsx
@@ -3,7 +3,6 @@ import actions from 'redux/actions'
 import { connect } from 'redaction'
 import { FormattedMessage } from 'react-intl'
 import cssModules from 'react-css-modules'
-import cx from 'classnames'
 import styles from './WalletConnectAccount.scss'
 
 import {
@@ -22,15 +21,21 @@ import WidthContainer from 'components/layout/WidthContainer/WidthContainer'
   dashboardModalsAllowed,
   metamaskData,
 }))
-@cssModules(styles)
+@cssModules(styles, { allowMultiple: true })
 class WalletConnectAccount extends React.Component<any, null> {
 
   handleClose = () => {
-  const {
-    name,
-  } = this.props
+    const {
+      name,
+    } = this.props
 
-  actions.modals.close(name)
+    actions.modals.close(name)
+  }
+
+  handleConnect = () => {
+    metamask.handleConnectMetamask({
+      dontRedirect: true,
+    })
   }
 
   handleDisconnect = () => {
@@ -41,47 +46,50 @@ class WalletConnectAccount extends React.Component<any, null> {
   render() {
     const {
         dashboardModalsAllowed,
-        metamaskData
+        metamaskData: {
+          isConnected,
+          address,
+          balance,
+          currency,
+        },
     } = this.props
 
     const web3Type = metamask.web3connect.getInjectedType()
     console.log('config.evmNetworks', config.evmNetworks)
 
     return (
-      <div
-        className={cx({
-          [styles['modal-overlay']]: true,
-          [styles['modal-overlay_dashboardView']]: dashboardModalsAllowed,
-        })}
-      >
-        <div
-          className={cx({
-            [styles['modal']]: true,
-            [styles['modal_dashboardView']]: dashboardModalsAllowed,
-          })}
-        >
+      <div styleName={`modal-overlay ${dashboardModalsAllowed ? "modal-overlay_dashboardView" : ""}`}>
+        <div styleName={`modal ${dashboardModalsAllowed ? "modal_dashboardView" : ""}`}>
           <div styleName="header">
             {/*//@ts-ignore */}
             <WidthContainer styleName="headerContent">
               <div styleName="title">
                 <FormattedMessage
                   id="WalletConnectAccountTitle"
-                  defaultMessage="WALLET ACCOUNT"
+                  defaultMessage="CONNECTED ACCOUNT"
                 />
               </div>
             </WidthContainer>
           </div>
           <div styleName="content">
             <div>
-              <p>Address: {metamaskData.address} </p>
-              <p>Balance: {`${metamaskData.balance} ${metamaskData.currency}`} </p>
-              <p>Network: {config.evmNetworks[metamaskData.currency].chainName}</p>
+              <p>Address: {address} </p>
+              <p>Balance: {`${balance} ${currency}`} </p>
+              <p>Network: {config.evmNetworks[currency].chainName}</p>
               <p>Wallet: {web3Type}</p>
             </div>
             <div styleName="button-overlay">
-              <Button blue onClick={this.handleDisconnect}>
-                <FormattedMessage id="MetamaskDisconnect" defaultMessage="Disconnect wallet" />
-              </Button>
+              {
+                isConnected ? (
+                  <Button blue onClick={this.handleDisconnect}>
+                    <FormattedMessage id="MetamaskDisconnect" defaultMessage="Disconnect wallet" />
+                  </Button>
+                ) : (
+                  <Button blue onClick={this.handleConnect}>
+                    <FormattedMessage id="ConnectWeb3Wallet" defaultMessage="Сonnect Wallet" />
+                  </Button>
+                )
+              }
               <Button blue onClick={this.handleClose}>
                 Cancel
               </Button>
