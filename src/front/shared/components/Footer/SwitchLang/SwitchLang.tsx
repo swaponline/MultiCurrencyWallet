ae6d901fbbfbcb246a2cc3ae859391f43c3633c9
NotEternal <cent1pede@protonmail.ch>
2021-09-26 11:32:44 +0200

    Refactoring

--- src/front/shared/components/Footer/SwitchLang/SwitchLang.tsx
+++ src/front/shared/components/Footer/SwitchLang/SwitchLang.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import styles from './SwitchLang.scss'
 import CSSModules from 'react-css-modules'
 
@@ -6,8 +5,12 @@ import { setCookie } from 'helpers/utils'
 import { FormattedMessage, injectIntl } from 'react-intl'
 import feedback from 'helpers/feedback'
 
+const DEFAULT_LANGUAGE = window.DEFAULT_LANGUAGE
+
 const SwitchLang = (props) => {
-  const { intl: { locale: intlLocale } } = props
+  const {
+    intl: { locale: intlLocale },
+  } = props
 
   const switchLang = (event, locale) => {
     event.preventDefault()
@@ -22,82 +25,55 @@ const SwitchLang = (props) => {
   }
 
   const localeIsNotMatched = (locale) => {
-    return intlLocale.toUpperCase() !== locale
-      ? true
-      : undefined // if url is undefined then we don't define styles
+    return intlLocale.toUpperCase() !== locale ? true : undefined // if url is undefined then we don't define styles
   }
+
+  const itemInfo = [
+    {
+      name: 'EN',
+      messageId: 'SwitchLang20',
+    },
+    {
+      name: 'RU',
+      messageId: 'SwitchLang24',
+    },
+    {
+      name: 'NL',
+      messageId: 'SwitchLangNL',
+    },
+    {
+      name: 'ES',
+      messageId: 'SwitchLangES',
+    },
+    {
+      name: 'DE',
+      messageId: 'SwitchLangDE',
+    },
+    {
+      name: 'PL',
+      messageId: 'SwitchLangPL',
+    },
+  ]
 
   return (
     <div styleName="langSwitcher">
-      <a
-        href={localeIsNotMatched('EN') && '#/'}
-        styleName="language"
-        onClick={(e) => {
-          switchLang(e, 'EN')
-          return false
-        }}
-      >
-        <FormattedMessage id="SwitchLang20" defaultMessage="EN" />
-      </a>
-      |
-      <a
-        href={localeIsNotMatched('RU') && '#/'}
-        styleName="language"
-        onClick={(e) => {
-          switchLang(e, 'RU')
-          return false
-        }}
-      >
-        <FormattedMessage id="SwitchLang24" defaultMessage="RU" />
-      </a>
-      |
-      <a
-        href={localeIsNotMatched('NL') && '#/'}
-        styleName="language"
-        onClick={(e) => {
-          switchLang(e, 'NL')
-          return false
-        }}
-      >
-        <FormattedMessage id="SwitchLangNL" defaultMessage="NL" />
-      </a>
-      |
-      <a
-        href={localeIsNotMatched('DE') && '#/'}
-        styleName="language"
-        onClick={(e) => {
-          switchLang(e, 'DE')
-          return false
-        }}
-      >
-        <FormattedMessage id="SwitchLangDE" defaultMessage="DE" />
-      </a>
-      |
-      <a
-        href={localeIsNotMatched('ES') && '#/'}
-        styleName="language"
-        onClick={(e) => {
-          switchLang(e, 'ES')
-          return false
-        }}
-      >
-        <FormattedMessage id="SwitchLangES" defaultMessage="ES" />
-      </a>
-      |
-      <a
-        href={localeIsNotMatched('PL') && '#/'}
-        styleName="language"
-        onClick={(e) => {
-          switchLang(e, 'PL')
-          return false
-        }}
-      >
-        <FormattedMessage id="SwitchLangPL" defaultMessage="PL" />
-      </a>
+      {itemInfo.map((info, index) => {
+        return (
+          <a
+            key={index}
+            href={localeIsNotMatched(info.name) && '#/'}
+            styleName="language"
+            onClick={(e) => {
+              switchLang(e, info.name)
+              return false
+            }}
+          >
+            <FormattedMessage id={info.messageId} defaultMessage={info.name} />
+          </a>
+        )
+      })}
     </div>
   )
 }
 
-export default injectIntl(
-  CSSModules(SwitchLang, styles, { allowMultiple: true })
-)
+export default injectIntl(CSSModules(SwitchLang, styles, { allowMultiple: true }))
