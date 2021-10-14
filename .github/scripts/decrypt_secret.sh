d3b2fcf31dc5b7ac80b62e9aeb5755227c1e9ad4
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-10-14 18:58:19 +0600

    tests

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -3,4 +3,4 @@
 # Decrypt the testWallets.json file for tests
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_FOR_TESTS" \
---output /home/runner/work/MultiCurrencyWallet/MultiCurrencyWallet/tests/testWallets.json /home/runner/work/MultiCurrencyWallet/MultiCurrencyWallet/tests/testWallets.json.gpg
+--output ./tests/testWallets.json ./tests/testWallets.json.gpg
