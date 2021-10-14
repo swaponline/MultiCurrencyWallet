fe65b1cb81f6c751ad2a3b92aa1fdcf4157c6595
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-10-14 18:29:34 +0600

    fix path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,4 +2,4 @@
 
 # Decrypt the testWallets.json file for tests
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_FOR_TESTS" \
---output $HOME/tests/testWallets.json $HOME/tests/testWallets.json.gpp
+--output ./tests/testWallets.json ./tests/testWallets.json.gpp
