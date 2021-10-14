2410cf588a58bb125a45d9748c1c12e98fbaf88b
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-10-14 18:35:43 +0600

    fix path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,9 +2,5 @@
 
 # Decrypt the testWallets.json file for tests
 
-pwd
-
-ls
-
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_FOR_TESTS" \
---output ./tests/testWallets.json ./tests/testWallets.json.gpp
+--output /home/runner/work/MultiCurrencyWallet/MultiCurrencyWallet/tests/testWallets.json /home/runner/work/MultiCurrencyWallet/MultiCurrencyWallet/tests/testWallets.json.gpp
