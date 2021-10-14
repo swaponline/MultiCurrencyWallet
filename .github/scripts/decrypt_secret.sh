d53d6116588df17b65803bfb7031082df45ac8b9
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-10-14 19:05:05 +0600

    print secret

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,5 +4,7 @@
 
 cat ./tests/testWallets.json
 
+echo "$SECRET_PASSPHRASE_FOR_TESTS"
+
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_FOR_TESTS" \
 --output ./tests/testWallets.json ./tests/testWallets.json.gpg
