e2e69c7122a08a5b108c3fac728a0aed300eea29
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-10-14 19:14:33 +0600

    fix workflow

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -4,7 +4,7 @@
 
 cat ./tests/testWallets.json
 
-echo "$SECRET_PASSPHRASE"
-
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output ./tests/testWallets.json ./tests/testWallets.json.gpg
+
+cat ./tests/testWallets.json
