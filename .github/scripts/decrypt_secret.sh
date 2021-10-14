4ca34d0f168772cfcd35758c9110d1e1121236f4
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-10-14 18:33:51 +0600

    check path

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 # Decrypt the testWallets.json file for tests
+
+pwd
+
+ls
+
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE_FOR_TESTS" \
 --output ./tests/testWallets.json ./tests/testWallets.json.gpp
