3c3f1c0920982dc04cc34139a43dbaaa408a57c7
Vitaliy Shulik <shulikvitalya@gmail.com>
2021-10-14 20:16:39 +0600

    add production test wallets

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,9 +2,6 @@
 
 # Decrypt the testWallets.json file for tests
 
-cat ./tests/testWallets.json
 
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" \
 --output ./tests/testWallets.json ./tests/testWallets.json.gpg
-
-cat ./tests/testWallets.json
