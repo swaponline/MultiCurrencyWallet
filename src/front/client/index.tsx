227093e5d95ea7abb402ccffa9815b3277f3b4de
Vitaliy Shulik <shulikvitalya@gmail.com>
2022-03-16 17:06:12 +0600

    remove sentry

--- src/front/client/index.tsx
+++ src/front/client/index.tsx
@@ -1,9 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 import routes from 'shared/routes'
 import store, { history } from 'redux/store'
-import * as Sentry from '@sentry/react';
-import { Integrations } from "@sentry/tracing";
 
 import Root from 'shared/containers/Root/Root'
 import { migrate } from 'helpers'
@@ -12,46 +9,25 @@ import config from 'app-config'
 import isLocalIP from 'is-local-ip'
 import feedback from 'shared/helpers/feedback'
 
-
 // eslint-disable-next-line camelcase
 const __webpack_public_path__ = `${config.publicPath}images/` // It makes webpack-require-from plugin works. So dont delete it.
 // All references is in swap.reace/webpack/rules/images.js
 
-
 const rootEl = document.getElementById('root')
 
 if (
-  (!config.isWidget || window?.STATISTICS_ENABLED) &&
-  process.env.NODE_ENV !== 'development'
-) {
-  Sentry.init({
-    dsn: 'https://d35a56c4518c4a6987b0c36b9b0bf123@sentry.wpmix.net/2',
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
-  });
-}
-
-if (
-  window.location.protocol === 'http:' &&
-  window.location.hostname !== 'localhost' &&
-  !isLocalIP(window.location.hostname)
+  window.location.protocol === 'http:'
+  && window.location.hostname !== 'localhost'
+  && !isLocalIP(window.location.hostname)
 ) {
   ReactDOM.render(
-    <Sentry.ErrorBoundary>
-      <ErrorPageNoSSL />
-    </Sentry.ErrorBoundary>
-    ,
-    rootEl
+    <ErrorPageNoSSL />,
+    rootEl,
   )
 } else {
   migrate().finally(() => setTimeout(() => {
-    ReactDOM.render(
-      <Sentry.ErrorBoundary>
-        {/* @ts-ignore: strictNullChecks */}
-        <Root history={history} store={store} routes={routes} />
-      </Sentry.ErrorBoundary>,
-      rootEl
-    )
+    // @ts-ignore: strictNullChecks
+    ReactDOM.render(<Root history={history} store={store} routes={routes} />, rootEl)
     console.log('(index.tsx) render root, finished')
   }, 1000))
 }
