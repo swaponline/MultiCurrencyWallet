a048e7be9620a0ae3198ede798c805332c3d614a
Vitaliy Shulik <shulikvitalya@gmail.com>
2022-04-06 11:31:11 +0600

    fix build errors

--- src/core/swap.app/util/erc20aurora.ts
+++ src/core/swap.app/util/erc20aurora.ts
@@ -3,12 +3,12 @@ import { BLOCKCHAIN as BLOCKCHAIN_TYPE, COIN_DATA, COIN_TYPE, TOKEN_STANDARD } f
 import typeforce from './typeforce'
 
 const register = (code, precision) => {
-  const tokenCode = `{${BLOCKCHAIN_TYPE.AURORA}}${code}`
+  const tokenCode = `{${BLOCKCHAIN_TYPE.AURETH}}${code}`
   constants.COINS[tokenCode] = tokenCode.toUpperCase()
   constants.COIN_DATA[tokenCode.toUpperCase()] = {
     ticker: tokenCode.toUpperCase(),
     name: code.toUpperCase(),
-    blockchain: BLOCKCHAIN_TYPE.AURORA,
+    blockchain: BLOCKCHAIN_TYPE.AURETH,
     standard: TOKEN_STANDARD.ERC20AURORA,
     type: COIN_TYPE.AURORA_TOKEN,
     model: COIN_DATA.AURETH.model,
