7aa42dcf250774e3c2125bbfc2e3bb30accc8ff4
samisbakedham <samsafahi@gmail.com>
2022-06-29 00:20:25 -0700

    Update erc20cndl.js

--- src/front/config/testnet/erc20cndl.js
+++ src/front/config/testnet/erc20cndl.js
@@ -1,8 +1,1 @@
-export default {
-  wbtc: {
-    address: '0xeb8df6700e24802a5d435e5b0e4228065ca9e0f3',
-    decimals: 8,
-    fullName: 'Wrapped Bitcoin',
-    canSwap: true,
-  },
-}
+export default {}
