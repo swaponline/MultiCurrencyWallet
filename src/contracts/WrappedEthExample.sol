8a233d09e8fa59747a19092807a46033ad1509f5
shendel <im_shendel@inbox.ru>
2022-09-16 21:17:16 +0300

    example of wrapped native coin contract

--- src/contracts/WrappedEthExample.sol
+++ src/contracts/WrappedEthExample.sol
@@ -20,8 +20,8 @@
 pragma solidity ^0.4.18;
 
 contract WETH9 {
-    string public name     = "Wrapped Ether";
-    string public symbol   = "WETH";
+    string public name     = "Wrapped PHIv2";
+    string public symbol   = "WPHI2";
     uint8  public decimals = 18;
 
     event  Approval(address indexed src, address indexed guy, uint wad);
