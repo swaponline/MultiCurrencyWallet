bb4755118195176b03d3ad242df51abb4cedf7a3
shendel <im_shendel@inbox.ru>
2022-10-12 15:54:35 +0300

    atomic swap contracts

--- src/front/config/testnet/erc20.js
+++ src/front/config/testnet/erc20.js
@@ -1,26 +1,14 @@
 export default {
-  swap: {
-    address: '0xbaa3fa2ed111f3e8488c21861ea7b7dbb5a7b121',
-    decimals: 18,
-    fullName: 'SWAP',
-    canSwap: false
-  },
-  usdt: {
-    address: '0xaf3c38a810670786d2fbd1a40adea7f9dc6e8746',
-    decimals: 6,
-    fullName: 'Tether',
-    canSwap: true
-  },
   weenus: {
-    address: '0xaff4481d10270f50f203e0763e2597776068cbc5',
+    address: '0xaFF4481D10270F50f203E0763e2597776068CBc5',
     decimals: 18,
     fullName: 'Weenus',
     canSwap: true
   },
-  wbtc: {
-    address: '0x0ec3461704e9c44741c9e11c7784e71a208388dd',
-    decimals: 8,
-    fullName: 'Wrapped Bitcoin',
-    canSwap: true,
-  },
+  weth: {
+    address: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+    decimals: 18,
+    fullName: 'Wrapped ETH',
+    canSwap: true
+  }
 }
--- src/front/config/testnet/web3.js
+++ src/front/config/testnet/web3.js
@@ -1,5 +1,5 @@
 export default {
-  provider: 'https://rinkeby.infura.io/v3/5ffc47f65c4042ce847ef66a3fa70d4c',
+  provider: 'https://goerli.infura.io/v3/5ffc47f65c4042ce847ef66a3fa70d4c',
   binance_provider: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
   matic_provider: 'https://rpc-mumbai.maticvigil.com',
   arbitrum_provider: 'https://rinkeby.arbitrum.io/rpc',
