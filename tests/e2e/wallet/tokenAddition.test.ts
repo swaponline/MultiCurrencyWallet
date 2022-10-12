1ff85fdb44f700563cabedbadffe084db7af703d
shendel <im_shendel@inbox.ru>
2022-10-12 18:31:00 +0300

    revert weth (mainnet)

--- tests/e2e/wallet/tokenAddition.test.ts
+++ tests/e2e/wallet/tokenAddition.test.ts
@@ -22,7 +22,7 @@ describe('Adding custom tokens', () => {
       'Custom ETH ERC20',
       'etherc20',
       {
-        contract: '0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6',
+        contract: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
         titleId: 'erc20weth',
         walletTitle: 'WETH ERC20',
       },
