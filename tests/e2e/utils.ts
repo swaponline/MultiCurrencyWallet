609cb1b31d131adba03e92f9febbd402e2f66aa9
shendel <im_shendel@inbox.ru>
2022-10-29 21:17:16 +0300

    check only token add

--- tests/e2e/utils.ts
+++ tests/e2e/utils.ts
@@ -17,7 +17,7 @@ if (process.env.ACTIONS) {
 console.log('>>>> START TEST', process.env.NODE_ENV)
 // if it's true then you will be able to see puppeteer's browser
 // don't enable this mode in Github test flows. They don't work with that
-const isDebug = false
+const isDebug = true
 
 export const createBrowser = async (): Promise<{
   browser: puppeteer.Browser
